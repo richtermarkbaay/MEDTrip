f6fd4974a5d8ddaa6eeb21ec139784c006e40035
alniejacobe <alnie.jacobe@chromedia.com>
2012-07-12 17:55:03 +0800

    updated controller for token creation

--- src/HealthCareAbroad/ProviderBundle/Controller/TokenController.php
+++ src/HealthCareAbroad/ProviderBundle/Controller/TokenController.php
@@ -5,80 +5,83 @@ namespace HealthCareAbroad\ProviderBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+
 use HealthCareAbroad\HelperBundle\Classes\Tokenizer;
-use HealthCareAbroad\HelperBundle\Classes\CheckExpirationDate;
 use HealthCareAbroad\HelperBundle\Entity\InvitationToken;
-
+use HealthCareAbroad\ProviderBundle\Entity\ProviderInvitation;
 class TokenController extends Controller
-{    
-
-	// public function indexAction()
-// 	{
-// 		 return $this->render('ProviderBundle:Token:index.html.twig');
-// 	}
+{
 	
     public function createAction(Request $request)
     {
-    	$request = $this->getRequest();		
-    	if ($request->getMethod() == 'POST') 
+    	$providerInvitation = new ProviderInvitation();
+        $form = $this->createFormBuilder($providerInvitation)
+            ->add('name', 'text')
+            ->add('email', 'text')
+            ->getForm();
+    	$request = $this->getRequest();	
+    		
+    	if ($request->getMethod() == 'POST')
 		{
-    		$tokenizer = new Tokenizer();
-    		$generatedToken = $tokenizer->generateTokenString();
-    	
-    		$expirationDate = new \DateTime('now');
-    		$expirationDate->modify('+6 days');
-			$status = "1";
+			$form->bindRequest($request);
 			
-    		$invitationToken = new InvitationToken();
-    		$invitationToken->setToken($generatedToken);
-    		$invitationToken->setExpirationDate($expirationDate);
-    		$invitationToken->setStatus($status);
-    		$em = $this->getDoctrine()->getEntityManager();
-    		$em->persist($invitationToken);
-    		$em->flush();
-    		
-    		//$checkToken = $this->get('CheckExpirationDate')->checkExpiredDateToken();
-    		return new Response('Created token! and '.$checkToken .' token with Expired dates');
-    		
-    		//send email
-    		
-    	}
-    	return $this->render('ProviderBundle:Token:create.html.twig');
-    }
-    /*public function checkAction(Request $request)
-    {
-    	//$generatedToken = $checkToken->checkforExpirationDate("2012-07-15 08:20:38");
-    	$request = $this->getRequest();		
-    	if ($request->getMethod() == 'POST') 
-		{
-			//$expirationDate = date_create(date('2012-07-15 08:20:38'));
-			$expirationDate = new \DateTime('now');
-			$repository = $this->getDoctrine()
-    						   ->getRepository('HelperBundle:InvitationToken');
-    						   
-			//select all token that has expired date and status is still active/1
-			$query = $repository->createQueryBuilder('t')
-    							->where('t.expiration_date < :expirationDate')
-    							->add('where', 't.expirationDate < :expirationDate and t.status = 1')
-    							->setParameter('expirationDate', $expirationDate)
-    							->getQuery();
+			if($form->isValid())
+			{
+				$data = $request->request->all();
+				$name = $data['form']['name'];
+				$email = $data['form']['email'];
+				
+				$tokenizer = new Tokenizer();
+				$generatedToken = $tokenizer->generateTokenString();
+				
+				$expirationDate = new \DateTime('now');
+				$expirationDate->modify('+6 days');
+				$status = "1";
+				
+				$invitationToken = new InvitationToken();
+				$invitationToken->setToken($generatedToken);
+				$invitationToken->setExpirationDate($expirationDate);
+				$invitationToken->setStatus($status);
+				$em = $this->getDoctrine()->getEntityManager();
+				$em->persist($invitationToken);
+				$em->flush();
+				
+				
 
-			$token = $query->getResult();    		
-			if (!$token) {
-        		throw $this->createNotFoundException('No token found with expirationDate '.$expirationDate);
-    		}
-    		else
-    		{	
-    			for($i = 0; $i < count($token); $i++){
-    				$token[$i]->setStatus('FALSE'); 
-    			}
-    			$em = $this->getDoctrine()->getEntityManager();
-    			$em->flush();
-    			return new Response("Updated!");
-    		}
-    		
-		}  
-    	return $this->render('ProviderBundle:Token:check.html.twig');
-    }*/
+
+
+				//send email
+				 $message = \Swift_Message::newInstance()
+ 					 ->setSubject('Activate your account with HealthCareAbroad')
+ 					 ->setFrom('alnie.jacobe@chromedia.com')
+ 					 ->setTo($email)
+ 					 ->setBody($this->renderView('ProviderBundle:Email:providerInvitationEmail.html.twig', array(
+ 					 			'name' => $name,
+ 					 			'expirationDate' => $expirationDate,
+ 					 			'email' => $email,
+ 					 			'token' => $generatedToken)));
+ 				$this->get('mailer')->send($message);
+ 	
+ 				$providerInvitation = new ProviderInvitation();
+ 				$providerInvitation->setEmail($email);
+ 				$providerInvitation->setMessage($message);
+ 				$providerInvitation->setName($name);
+ 				$providerInvitation->setStatus($status);
+ 				//$providerInvitation->setInvitationToken($generatedToken);
+ 				$em = $this->getDoctrine()->getEntityManager();
+ 				$em->persist($providerInvitation);
+ 				$em->flush();
+				
+				//$checkToken = $this->get('CheckExpirationDate')->checkExpiredDateToken();
+				return new Response('Created token! and send invitation token to recipient');
+			}
+			return $this->render('ProviderBundle:Token:create.html.twig', array(
+            	'form' => $form->createView(),
+        	));
+		}
+    	return $this->render('ProviderBundle:Token:create.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }    
     
 }
