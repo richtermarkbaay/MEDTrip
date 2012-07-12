f0d3d830cd4d9f1911664d8b0ac5a267c0abe32e
Harold Modesto <harold.modesto@chromedia.com>
2012-07-12 14:44:05 +0800

    removed references to couchdb

--- src/HealthCareAbroad/ListingBundle/DependencyInjection/Configuration.php
+++ src/HealthCareAbroad/ListingBundle/DependencyInjection/Configuration.php
@@ -24,12 +24,6 @@ class Configuration implements ConfigurationInterface
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
 
-       	$rootNode
-        	->children()
-        	->booleanNode('use_couchdb')->defaultValue(true)->end()
-        	->end()
-        	;
-        
         return $treeBuilder;
     }
 }
