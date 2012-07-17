dfe315660633433bc31996cebbc044a85a7bc948
Chris <chris.velarde@chromedia.com>
2012-07-17 17:15:29 +0800

    moved sending of user invitation to a service

--- src/HealthCareAbroad/HelperBundle/Services/InvitationService.php
+++ src/HealthCareAbroad/HelperBundle/Services/InvitationService.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace HealthCareAbroad\HelperBundle\Services;
+use HealthCareAbroad\ProviderBundle\Entity\ProviderUserInvitation;
+
+use HealthCareAbroad\ProviderBundle\Entity\Provider;
+
 use HealthCareAbroad\HelperBundle\Entity\InvitationToken;
 use HealthCareAbroad\ProviderBundle\Entity\ProviderInvitation;
 use ChromediaUtilities\Helpers\SecurityHelper;
@@ -11,22 +15,41 @@ class InvitationService
 {
 	protected $doctrine;
 	
+	/**
+	 * 
+	 * @var EntityManager
+	 */
+	protected $em;
+	
+	/**
+	 * 
+	 * @var \Twig_Environment
+	 */
+	protected $twig;
+	
 	public function __construct(EntityManager $em)
 	{
 		$this->em = $em;
 	}
+	
+	public function setTwig(\Twig_Environment $twig)
+	{
+	    $this->twig = $twig;
+	}
+	
 	public function createInvitationToken($expirationDate)
 	{
 		$generatedToken = SecurityHelper::hash_sha256(date('Ymdhms'));
 		
 		$invitationToken = new InvitationToken();
 		$invitationToken->setToken($generatedToken);
-		$invitationToken->setExpirationDate($expirationDate);
+		$invitationToken->setExpirationDate(new \DateTime('+30 days'));
 		$invitationToken->setStatus("1");
 		
  		$this->em->persist($invitationToken);
  		$this->em->flush();
- 		return $generatedToken;
+ 		
+ 		return $invitationToken;
 	}
 	
 	
@@ -41,6 +64,35 @@ class InvitationService
 		
 		$this->em->persist($providerInvitation);
 		$this->em->flush();
-		
-	}	
+	}
+	
+	public function sendProviderUserInvitation(Provider $provider, ProviderUserInvitation $invitation)
+	{
+	    if (!$token = $invitation->getInvitationToken()) {
+	        // generate a token
+	        $token = $this->createInvitationToken(30);
+	        $invitation->setInvitationToken($token);
+	    }
+	    
+	    // persist invitation to database
+	    $this->em->persist($invitation);
+	    $this->em->flush();
+	    $messageBody = $this->twig->render('ProviderBundle:Email:invite.email.twig', array(
+            'providerUserInvitation' => $invitation,
+            'token' => $token,
+            'provider' => $provider
+        ));
+	    
+	    // send to email
+	    $message = \Swift_Message::newInstance()
+	    ->setSubject('Provider User Invitation for Health Care Abroad')
+	    ->setFrom('chaztine.blance@chromedia.com')
+	    ->setTo($invitation->getEmail())
+	    ->setBody($messageBody);
+	    
+	    //$this->mailer->send($message);
+	    
+	    
+	    return true;
+	}
 }
--- src/HealthCareAbroad/ProviderBundle/Controller/ProviderUserController.php
+++ src/HealthCareAbroad/ProviderBundle/Controller/ProviderUserController.php
@@ -2,6 +2,8 @@
 
 namespace HealthCareAbroad\ProviderBundle\Controller;
 
+use HealthCareAbroad\ProviderBundle\Entity\ProviderUserInvitation;
+
 use ChromediaUtilities\Helpers\SecurityHelper;
 
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
@@ -21,10 +23,10 @@ class ProviderUserController extends Controller
             ->add('email', 'email', array('property_path'=> false))
             ->add('password', 'password', array('property_path'=> false))
             ->getForm();
-        $request = $this->getRequest();
-        if ($request->isMethod('POST')) {
+        
+        if ($this->getRequest()->isMethod('POST')) {
             
-            $form->bindRequest($request);
+            $form->bindRequest($this->getRequest());
             if ($form->isValid()) {
                 
                 $user->setEmail($form->get('email')->getData());
@@ -59,6 +61,46 @@ class ProviderUserController extends Controller
         $this->get('security.context')->setToken(null);
         $this->getRequest()->getSession()->invalidate();
         return $this->redirect($this->generateUrl('main_homepage'));
+    }
+    
+    public function inviteAction()
+    {
+        $provider = $this->get('services.provider')->getCurrentProvider();
+        
+        $providerUserInvitation = new ProviderUserInvitation();
+        $providerUserInvitation->setProvider($provider);
+        $providerUserInvitation->setDateCreated(new \DateTime('now'));
+        
+        $form = $this->createFormBuilder($providerUserInvitation)
+            ->add('email', 'email')
+            ->add('message', 'textarea')
+            ->add('firstName', 'text')
+            ->add('middleName', 'text')
+            ->add('lastName', 'text')
+            ->getForm();
+        
+        if ($this->getRequest()->isMethod('POST')) {
+            
+            $form->bindRequest($this->getRequest());
+            
+            if ($form->isValid()){
+                
+                $sendingResult = $this->get('services.invitation')->sendProviderUserInvitation($provider, $providerUserInvitation);
+                
+                if ($sendingResult) {
+                    $this->get('session')->setFlash('flash.notice', "Invitation sent to {$providerUserInvitation->getEmail()}");
+                }
+                else {
+                    $this->get('session')->setFlash('flash.notice', "Failed to send invitation to {$providerUserInvitation->getEmail()}");
+                }
+                
+                return $this->redirect($this->generateUrl('provider_invite_user'));
+            }
+        }
+        
+        return $this->render('ProviderBundle:Default:invite.html.twig', array(
+                        'form' => $form->createView(),
+        ));
     }
     
 }
