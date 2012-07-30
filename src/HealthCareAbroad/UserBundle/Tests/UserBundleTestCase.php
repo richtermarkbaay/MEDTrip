34badc7863604df7af0853467ef237892f2edb1e
Chris <chris.velarde@chromedia.com>
2012-07-30 08:41:41 +0800

    removed unnecessary variables in UserBundleTestCase

--- src/HealthCareAbroad/UserBundle/Tests/UserBundleTestCase.php
+++ src/HealthCareAbroad/UserBundle/Tests/UserBundleTestCase.php
@@ -5,10 +5,6 @@ namespace HealthCareAbroad\UserBundle\Tests;
 
 abstract class UserBundleTestCase extends \PHPUnit_Framework_TestCase
 {
-    protected static $kernel;
-    
-    protected static $container;
-    
     public static function setUpBeforeClass()
     {
         \HCA_DatabaseManager::getInstance()->restoreDatabaseState();
