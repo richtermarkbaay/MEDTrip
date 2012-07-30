af7e3ed24e4c94fca852408ae1f79f2c9df982ca
Chris <chris.velarde@chromedia.com>
2012-07-30 08:45:59 +0800

    added ProviderUserInvitationType

---
+++ src/HealthCareAbroad/ProviderBundle/Form/ProviderUserInvitationType.php
@@ -0,0 +1,32 @@
+<?php
+namespace HealthCareAbroad\ProviderBundle\Form;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProviderUserInvitationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('email', 'email')
+            ->add('message', 'textarea')
+            ->add('firstName', 'text')
+            ->add('middleName', 'text')
+            ->add('lastName', 'text');
+    }
+    
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'HealthCareAbroad\ProviderBundle\Entity\ProviderUserInvitation',
+        ));
+    }
+    
+    public function getName()
+    {
+        return 'providerUserInvitation';
+    }
+}
