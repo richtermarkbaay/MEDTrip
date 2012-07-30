2796cb4ff55c5d6700f38466e9ba8b34e50616e6
Chris <chris.velarde@chromedia.com>
2012-07-30 09:32:48 +0800

    renamed ProviderUserServiceTest to InstitutionUserServiceTest

--- src/HealthCareAbroad/UserBundle/Tests/UserBundleTestCase.php
+++ src/HealthCareAbroad/UserBundle/Tests/UserBundleTestCase.php
@@ -1,10 +1,15 @@
 <?php
 
-
 namespace HealthCareAbroad\UserBundle\Tests;
+
+use \HCA_DatabaseManager;
 
 abstract class UserBundleTestCase extends \PHPUnit_Framework_TestCase
 {
+    private $doctrine = null;
+    
+    private $container = null;
+    
     public static function setUpBeforeClass()
     {
         \HCA_DatabaseManager::getInstance()->restoreDatabaseState();
@@ -13,5 +18,24 @@ abstract class UserBundleTestCase extends \PHPUnit_Framework_TestCase
     public static function tearDownAfterClass()
     {
         \HCA_DatabaseManager::getInstance()->restoreDatabaseState();
+    }
+    
+    /**
+     * @return \Doctrine\Bundle\DoctrineBundle\Registry
+     */
+    public function getDoctrine()
+    {
+        if (\is_null($this->doctrine)) {
+            $this->doctrine = HCA_DatabaseManager::getInstance()->getDoctrine(); 
+        }
+        return $this->doctrine;
+    }
+    
+    public function getServiceContainer()
+    {
+        if (\is_null($this->container)) {
+            $this->container = \HCA_ServiceManager::getInstance()->getContainer();
+        }
+        return $this->container;
     }
 }
