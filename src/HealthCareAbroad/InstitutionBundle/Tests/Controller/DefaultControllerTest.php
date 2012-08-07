c91a8bf7ad0823b4f3e5b5a4e2959d4f2b8f199d
Allejo Chris Velarde <chris.velarde@chromedia.com>
2012-08-07 12:10:40 +0800

    added test for edit account flow

--- src/HealthCareAbroad/InstitutionBundle/Tests/Controller/DefaultControllerTest.php
+++ src/HealthCareAbroad/InstitutionBundle/Tests/Controller/DefaultControllerTest.php
@@ -14,5 +14,10 @@ class DefaultControllerTest extends InstitutionBundleWebTestCase
         $this->assertEquals(302, $client->getResponse()->getStatusCode());// expecting a redirect to login form
         $redirectLocation = $client->getResponse()->headers->get('location');
         $this->assertTrue($redirectLocation=='/institution/location' || $redirectLocation == 'http://localhost/institution/login');
+        
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $crawler = $client->request('GET', '/institution');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        
     }
 }
