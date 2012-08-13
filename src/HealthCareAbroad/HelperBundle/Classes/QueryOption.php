89f0484615d90851bb7afe388ed8921cd940aa08
Allejo Chris Velarde <chris.velarde@chromedia.com>
2012-08-13 09:25:55 +0800

    added QueryOption in helper bundle

---
+++ src/HealthCareAbroad/HelperBundle/Classes/QueryOption.php
@@ -0,0 +1,11 @@
+<?php
+namespace HealthCareAbroad\HelperBundle\Classes;
+
+final class QueryOption
+{
+    const ORDER_BY = 'order_by';
+    
+    const GROUP_BY = 'group_by';
+    
+    const LIMIT = 'limit';
+}
