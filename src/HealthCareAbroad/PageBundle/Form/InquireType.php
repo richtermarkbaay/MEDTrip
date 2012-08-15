122c5ad4ab39710fd4baf79a64da0a66ae1b5018
alniejacobe <alnie.jacobe@chromedia.com>
2012-08-15 09:18:38 +0800

    create inquire form, including its entities and repositories

--- src/HealthCareAbroad/PageBundle/Form/InquireType.php
+++ src/HealthCareAbroad/PageBundle/Form/InquireType.php
@@ -2,21 +2,29 @@
 
 namespace HealthCareAbroad\PageBundle\Form;
 
+use HealthCareAbroad\PageBundle\Form\ListType\InquireAboutListType;
+
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormViewInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class InquireType extends AbstractType
 {
+	private $container;
+	
+	public function setContainer(ContainerInterface $container = null) {
+		$this->container = $container;
+	}
 	public function buildForm(FormBuilderInterface $builder, array $options)
     {
     	$builder
     		->add('firstName', 'text', array('constraints' => new NotBlank()))
     		->add('lastName', 'text', array('constraints' => new NotBlank()))
     		->add('email','email', array('constraints' => array(new Email(), new NotBlank())))
-    		->add('subject', 'choice')
+    		->add('inquire_about', 'inquire_about_list')
     		->add('message', 'textarea', array('constraints' => new NotBlank()))
     		;
     }
---
+++ src/HealthCareAbroad/PageBundle/Services/InquireService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace HealthCareAbroad\PageBundle\Services;
+
+use HealthCareAbroad\AdminBundle\Entity\InquireAbout;
+use HealthCareAbroad\AdminBundle\Entity\Inquire;
+
+class InquireService
+{
+	protected $doctrine;
+
+	public function setDoctrine(\Doctrine\Bundle\DoctrineBundle\Registry $doctrine)
+	{
+		$this->doctrine = $doctrine;
+	}
+
+	public function getActiveInquireAbouts()
+	{
+		return $this->doctrine->getEntityManager()->createQueryBuilder()
+		->add('select', 'a')
+		->add('from', 'AdminBundle:InquireAbout a')
+		->add('where', 'a.status = 1');
+	}
+	
+	public function createInquire(Inquire $inquire)
+	{
+		$em = $this->doctrine->getEntityManager();
+		$em->persist($inquire);
+		$em->flush();
+		
+		// failed to save
+		if (!$inquire) {
+			return $this->_errorResponse(500, 'Exception encountered upon persisting data.');
+		}
+		
+		return $inquire;
+	}
+}
