92a697925baf941f40cab380d8be5df05dbca2dd
Allejo Chris Velarde <chris.velarde@chromedia.com>
2012-08-16 17:19:41 +0800

    implemented add/edit medical center

---
+++ src/HealthCareAbroad/MedicalProcedureBundle/Form/ListType/MedicalCenterListType.php
@@ -0,0 +1,29 @@
+<?php
+namespace HealthCareAbroad\MedicalProcedureBundle\Form\ListType;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class MedicalCenterListType extends AbstractType
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'property' => 'name',
+            'label' => 'Medical Center',
+            'class' => 'HealthCareAbroad\MedicalProcedureBundle\Entity\MedicalCenter'
+        ));
+    }
+     
+    public function getParent()
+    {
+        return 'entity';
+    }
+    
+    public function getName()
+    {
+        return 'medicalCenter_list';
+    }
+}
