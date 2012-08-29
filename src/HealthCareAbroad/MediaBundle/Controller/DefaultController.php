c92cae50859039e211da1c27faca5a59c620e633
Harold Modesto <harold.modesto@chromedia.com>
2012-08-29 17:17:22 +0800

    added twig extension and template helper for media

--- src/HealthCareAbroad/MediaBundle/Controller/DefaultController.php
+++ src/HealthCareAbroad/MediaBundle/Controller/DefaultController.php
@@ -46,34 +46,4 @@ class DefaultController extends Controller
 		
 		return $response->create('Error code: '.$errorCode);
 	}
-	
-	public function indexAction(Request $request) 
-	{
-		$filesystems = $this->get('chromedia_gaufrette.filesystem_map');
-
-		echo '<br/><br/>filesystem_map';
-		var_dump($filesystems);		
-		
-		$tmp = $this->get('chromedia_gaufrette.filesystem_map')->get('default');
-		echo '<br/><br/>local';
-		var_dump($tmp);
-		
-		$tmp = $this->get('chromedia_gaufrette.filesystem_map')->get('test');
-		echo '<br/><br/>test';
-		var_dump($tmp);
-		
-		
-		echo '<br/><br/>';
-		//echo $filesystems->get('test')->read('file2.txt');
-		$file = new File('file2.txt', $filesystems->get('test'));
-		echo $file->getContent();
-
-		
-		$file = new File('helloFile', $filesystems->get('default'));
-		$file->setContent('Hello World');
-		
-		echo $file->getContent(); // Hello World		
-		
-		exit;
-	}
 }
