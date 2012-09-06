5371cf9a7ee96886e1b48d5dae24b2ba0d06b622
alniejacobe <alnie.jacobe@chromedia.com>
2012-09-06 16:35:18 +0800

    added events for city,country,tag and inquiry

---
+++ src/HealthCareAbroad/AdminBundle/Listener/CityLogListener.php
@@ -0,0 +1,36 @@
+<?php 
+
+namespace HealthCareAbroad\AdminBundle\Listener;
+
+use HealthCareAbroad\AdminBundle\Events\CreateCityEvent;
+use Doctrine\ORM\EntityManager;
+
+class CityLogListener 
+{
+	/*
+	 * 
+	 * @var Doctrine\ORM\EntityManager 
+	 */
+	
+	private $em;
+	
+	public function setEntityManager(EntityManager $em)
+	{
+		$this->em = $em;
+	}
+	
+	public function onAdd(CreateCityEvent $event)
+	{
+		
+	}
+	
+	public function onEdit(CreateCityEvent $event)
+	{
+	
+	}
+	
+	public function onDelete(CreateCityEvent $event)
+	{
+	
+	}
+}
