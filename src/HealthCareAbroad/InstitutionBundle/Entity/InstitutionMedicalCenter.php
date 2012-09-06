0d91d3cfa6c76f423ba687b90dd9f95b050dbae2
Harold Modesto <harold.modesto@chromedia.com>
2012-09-06 14:54:48 +0800

    added constant STATUS_DRAFT

--- src/HealthCareAbroad/InstitutionBundle/Entity/InstitutionMedicalCenter.php
+++ src/HealthCareAbroad/InstitutionBundle/Entity/InstitutionMedicalCenter.php
@@ -9,9 +9,11 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class InstitutionMedicalCenter
 {
-	const STATUS_ACTIVE = 1;
-	
-	const STATUS_INACTIVE = 0;
+    const STATUS_DRAFT = 2;
+    
+    const STATUS_ACTIVE = 1;
+    
+    const STATUS_INACTIVE = 0;
 
     /**
      * @var bigint $id
