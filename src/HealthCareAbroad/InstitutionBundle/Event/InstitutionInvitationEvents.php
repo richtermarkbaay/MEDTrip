9110016a37444adbcdc32a64c81b28aae537fb17
alniejacobe <alnie.jacobe@chromedia.com>
2012-09-11 08:20:24 +0800

    fixed errors on editInstitution,added functional test and create another controller for loadCities and signUp for institution

--- src/HealthCareAbroad/InstitutionBundle/Event/InstitutionInvitationEvents.php
+++ src/HealthCareAbroad/InstitutionBundle/Event/InstitutionInvitationEvents.php
@@ -2,7 +2,7 @@
 
 namespace HealthCareAbroad\InstitutionBundle\Event;
 
-final class InstitutionInvitation
+final class InstitutionInvitationEvents
 {
 	const ON_ADD_INSTITUTION_INVITATION = 'event.institution_invitation.add';
 }
