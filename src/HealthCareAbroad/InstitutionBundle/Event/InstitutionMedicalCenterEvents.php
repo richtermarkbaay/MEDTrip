c293cada2348f82e3006746547105a7370ad948f
alniejacobe <alnie.jacobe@chromedia.com>
2012-09-13 12:10:55 +0800

    deleted some events

--- src/HealthCareAbroad/InstitutionBundle/Event/InstitutionMedicalCenterEvents.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace HealthCareAbroad\InstitutionBundle\Event;
-
-final class InstitutionMedicalCenterEvents
-{
-    const ON_ADD_INSTITUTION_MEDICAL_CENTER = 'event.institution_medical_center.add';
-    const ON_EDIT_INSTITUTION_MEDICAL_CENTER = 'event.institution_medical_center.edit';
-    const ON_DELETE_INSTITUTION_MEDICAL_CENTER = 'event.institution_medical_center.delete';
-}
--- src/HealthCareAbroad/InstitutionBundle/Event/InstitutionUserRoleEvents.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace HealthCareAbroad\InstitutionBundle\Event;
-
-final class InstitutionUserRoleEvents
-{
-    const ON_ADD_INSTITUTION_USER_ROLE= 'event.institution_user_role.add';
-    const ON_EDIT_INSTITUTION_USER_ROLE = 'event.institution_user_role.edit';
-    const ON_DELETE_INSTITUTION_USER_ROLE = 'event.institution_user_role.delete';
-}
