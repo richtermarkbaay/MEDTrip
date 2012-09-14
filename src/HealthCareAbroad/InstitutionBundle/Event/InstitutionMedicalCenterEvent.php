dd08e3c442e000cd734887d593a0a0b2d1ecb2f1
Adelbert Silla <adelbert.silla@chromedia.com>
2012-09-14 11:15:12 +0800

    Updated Institution Events Classes.

--- src/HealthCareAbroad/InstitutionBundle/Event/InstitutionMedicalCenterEvent.php
+++ src/HealthCareAbroad/InstitutionBundle/Event/InstitutionMedicalCenterEvent.php
@@ -1,22 +1,12 @@
 <?php 
 namespace HealthCareAbroad\InstitutionBundle\Event;
 
-use HealthCareAbroad\InstitutionBundle\Entity\InstitutionMedicalCenter;
+use HealthCareAbroad\HelperBundle\Event\BaseEvent;
 
-use Symfony\Component\EventDispatcher\Event;
-
-abstract class InstitutionMedicalCenterEvent extends Event
+class InstitutionMedicalCenterEvent extends BaseEvent
 {
-    protected $medicalCenter;
-    
-    public function __construct(InstitutionMedicalCenter $medicalCenter)
-    {
-        $this->medicalCenter = $medicalCenter;
-    }
-
     public function getInstitutionMedicalCenter()
     {
-        return $this->medicalCenter;
+        return $this->data;
     }
-    
 }
