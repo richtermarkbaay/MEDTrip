583dc7e461a7ada145aa4d6e82e3db887fce6ce3
Allejo Chris Velarde <chris.velarde@chromedia.com>
2012-09-14 16:13:35 +0800

    updated dispatched event in institution user role controller

---
+++ src/HealthCareAbroad/LogBundle/Listener/Institution/InstitutionUserTypeListener.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Log listener for InstitutionUserType events
+ * 
+ * @author Allejo Chris G. Velarde
+ *
+ */
+namespace HealthCareAbroad\LogBundle\Listener\Institution;
+
+use HealthCareAbroad\HelperBundle\Event\BaseEvent;
+
+use HealthCareAbroad\LogBundle\Listener\BaseListener;
+
+class InstitutionUserTypeListener extends BaseListener
+{
+    public function onAdd(BaseEvent $event)
+    {
+        
+    }
+    
+    public function onEdit(BaseEvent $event)
+    {
+    
+    }
+    
+    public function onDelete(BaseEvent $event)
+    {
+    
+    }
+    
+    public function onAddUserRole(BaseEvent $event)
+    {
+        
+    }
+    
+    public function onDeleteUserRole(BaseEvent $event)
+    {
+        
+    }
+} 
