a8dc1cb9f1f7fec7eb6346946b309b8c44b5daf0
Allejo Chris Velarde <chris.velarde@chromedia.com>
2012-09-18 12:05:11 +0800

    implemented event options

--- src/HealthCareAbroad/HelperBundle/Event/BaseEvent.php
+++ src/HealthCareAbroad/HelperBundle/Event/BaseEvent.php
@@ -6,11 +6,23 @@
  */
 namespace HealthCareAbroad\HelperBundle\Event;
 
+use Symfony\Component\HttpFoundation\ParameterBag;
+
 use Symfony\Component\EventDispatcher\Event;
 
 abstract class BaseEvent extends Event
 {
     protected $data;
+    
+    /**
+     * @var ParameterBag
+     */
+    private $options;
+    
+    public function __construct()
+    {
+        $this->options = new ParameterBag();
+    }
     
     final public function setData($data)
     {
@@ -22,5 +34,29 @@ abstract class BaseEvent extends Event
     final public function getData()
     {
         return $this->data; 
+    }
+    
+    /**
+     * @return ParameterBag
+     */
+    final public function getOptions()
+    {
+        return $this->options;
+    }
+    
+    /**
+     * Add an option for this event
+     * 
+     * @param string $key
+     * @param mixed $value
+     */
+    final public function addOption($key, $value)
+    {
+        $this->options->add(array($key => $value));
+    }
+    
+    final public function getOption($key, $default=null)
+    {
+        return $this->options->get($key, $default);
     }
 }
