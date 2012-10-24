36c36eac6f2136dfa014d88e9292ae5544a749b8
Hannah Panares <hannah.panares@chromedia.com>
2012-10-24 14:01:31 +0800

    added new layout and removed the old one for client admin

---
+++ web/markup/client-admin/inc.header.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Admin Health Care Abroad - Login Page</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="HTML5 Admin Health Care Abroad Template">
+<meta name="author" content="healthcare">
+
+ <!-- stylesheet -->
+  	<!--<link rel="stylesheet" href="css/bootstrap.css">
+  	<link rel="stylesheet" href="css/bootstrap-responsive.css">-->
+  	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" href="css/hca-style.css">
+
+ <!-- Le fav and touch icons -->
+  <link rel="shortcut icon" href="images/favicon.ico">
+
+
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+
+<body><!-- Top navigation bar -->
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container">
+		<a href="index.php" class="brand"><img src="images/HCA-logo-small.png" alt="Health Care Abroad Logo" longdesc="#"> HEALTHCARE ABROAD</a>
+			<div class="btn-group pull-right">
+       		     
+       		     <span class="userbox">you are logged in as<i class="icon-user"><a href="#"></i>marcjacob@gmail</a></span>
+       		     <a href="accountsettings.html"><img src="images/Setting-icon.png"></a>
+       		     <a href="logout.html"><img src="images/logout-icon.png"></a>
+       		 </div>
+       		 
+       		 
+    </div>
+  </div>
+</div>
+<div class="row-fluid">
+<div class="institutionbox">
+	<div class="container">
+		<div class="institutionbox-inner"> 
+			<div class="navbar-inner">
+	   	 		<div class="span6">
+	        		<div class="span2"><a href="#"><img class="member-box-avatar" src="http://images.goabroad.net/images/default_images/profile_photo.gif"></a></div>
+			        <div class="span7"><h3>Institution Name Here</h3></div>
+			    </div>
+		    	<div class="span5">
+	    			<a href="#"><h2>Manage Your Institution Profile</h2></a>
+	    			<!--<p><a href="#">[Manage your Institutional Profile here]</a></p>
+		    		<p><a href="#">[add your locations here]</a></p>
+		    		<p><a href="#">[add your awards, certification and affiliation]</a></p>
+	    			<h4><a href="adding-center.html">ADD YOUR MEDICAL CENTER here!</a></h4>-->
+	    			<p>+6332-2555555 (contact number)</p>
+	    			<p>Osmeña Boulevard Cebu City, Philippines 6000 (address)</p>
+	    			<p>www.site.com (address)</p>
+	    			<h4><a href="adding-center.html">ADD YOUR MEDICAL CENTER here!</a></h4>
+	    			
+	    		</div>
+
+	    </div>
+	</div> 
+</div>
+</div>
+<div class="container">
+	<div class="span12">
+	
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+	<script type="text/javascript">
+	function toggleDiv(divId) {
+	   $("#"+divId).toggle();
+	}
+	</script>
+	
+	<div id="box-1">
+	  <span class="span1"><a href="javascript:toggleDiv('myContent');"><img src="images/arrow.png"></a> </span>    
+	  
+	 
+	</div>
+	<div id="myContent">
+	
+	<div class="box-container-toggle stepbox">
+
+	<span class="span11"><h5>Thank you for registering on Health Care Abroad.com. If you are still new to things, we’ve provided a few walkthroughs to get you started.</h5></span> 	    	
+	    	<ul class="nav nav-tabs nav-stacked">
+	    		      
+	    	    <li><a href="#"><h1>1</h1><h5>Manage your Institution Profile</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum, est at congue gravida...</p></a></li>
+	    	    <li><a href="#"><h1>2</h1><h5>Add your Medical Center</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum, est at congue gravida...</p></a></li>
+	    	    <li><a href="#"><h1>3</h1><h5>Add your world Class Specialist (Doctors)</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum, est at congue gravida...</p></a></li>
+	    	    <li><a href="#"><h1>4</h1><h5>Uploads Photos, Videos etc. (Media Files)</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum, est at congue gravida...</p></a></li>
+	    	    <li><a href="#"><h1>5</h1><h5>Add your Affiliations, Awards & Certifications</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum, est at congue gravida...</p></a></li>
+	    	    	      	    
+	    	</ul> 
+	    	
+	    	
+	
+	    </div>
+	    <div class="clear"></div>
+	</div>
+	</div>
+	
+				
+	
+	</div>
+	</div>
