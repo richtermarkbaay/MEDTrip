f6356f31531f5179d3e22787be4fdbbbc180f677
Harold Modesto <harold.modesto@chromedia.com>
2012-11-05 13:41:44 +0800

    implemented buildQueryBuilder

--- src/HealthCareAbroad/HelperBundle/Services/Filters/InstitutionSpecializationListFilter.php
+++ src/HealthCareAbroad/HelperBundle/Services/Filters/InstitutionSpecializationListFilter.php
@@ -13,9 +13,26 @@ class InstitutionSpecializationListFilter extends ListFilter
     {
         $this->setStatusFilterOption();
     }
-    
+
     function buildQueryBuilder()
     {
-        return;
+        $this->queryBuilder->select('a')->from('InstitutionBundle:InstitutionSpecialization', 'a');
+        $this->queryBuilder->leftJoin('a.institutionMedicalCenter', 'b');
+        $this->queryBuilder->leftJoin('a.specialization', 'c');
+        $this->queryBuilder->where('b.institution = :institutionId');
+        $this->queryBuilder->setParameter('institutionId', $this->queryParams['institutionId']);
+
+        if ($this->queryParams['status'] != ListFilter::FILTER_KEY_ALL) {
+            $this->queryBuilder->andWhere('a.status = :status');
+            $this->queryBuilder->setParameter('status', $this->queryParams['status']);
+        }
+
+        if(!$this->sortBy || $this->sortBy === 'name') {
+            $sort = 'c.name ' . $this->sortOrder;
+        } else {
+            $sort = 'a.' . $this->sortBy. ' ' . $this->sortOrder;
+        }
+
+        $this->queryBuilder->add('orderBy', $sort);
     }
 }
