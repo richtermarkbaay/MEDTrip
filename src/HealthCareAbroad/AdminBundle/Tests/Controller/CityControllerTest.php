f7447ebfe103a9043691e88f38476efc509763fb
Allejo Chris Velarde <chris.velarde@chromedia.com>
2012-11-13 10:06:28 +0800

    updated city and country tests

--- src/HealthCareAbroad/AdminBundle/Tests/Controller/CityControllerTest.php
+++ src/HealthCareAbroad/AdminBundle/Tests/Controller/CityControllerTest.php
@@ -166,7 +166,8 @@ class CityControllerTest extends AdminBundleWebTestCase
         // Test Filter country and status
         $crawler = $client->request('GET', '/admin/cities?country1=&status=1');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $isFiltered = $crawler->filter('#city-list tr > td.country:not(:contains("Philippines"))')->count() == 0;
+        
+        $isFiltered = $crawler->filter('#city-list tr > td.country:not(:contains("Philippine"))')->count() == 0;
         $isAllActive = $crawler->filter('#city-list tr a.icon-5')->count() == 0;
         $this->assertEquals(true, $isFiltered && $isAllActive, 'Filter contry and status is not working properly!');
     }
