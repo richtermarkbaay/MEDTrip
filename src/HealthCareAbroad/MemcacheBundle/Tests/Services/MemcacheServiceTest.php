0595d9d36475c25c1aac0ac9a2a1dc18e6f96846
Harold Modesto <harold.modesto@chromedia.com>
2012-11-19 16:14:25 +0800

    reverted tests

--- src/HealthCareAbroad/MemcacheBundle/Tests/Services/MemcacheServiceTest.php
+++ src/HealthCareAbroad/MemcacheBundle/Tests/Services/MemcacheServiceTest.php
@@ -13,7 +13,7 @@ class MemcacheServiceTest extends MemcacheBundleUnitTestCase
 
     public function testSet()
     {
-        //$returnVal = $this->getServiceContainer()->get('services.memcache')->set($this->key, $this->value);
+        $returnVal = $this->getServiceContainer()->get('services.memcache')->set($this->key, $this->value);
 
         $this->assertInstanceOf('HealthCareAbroad\MemcacheBundle\Services\MemcacheService', $returnVal, 'Expecting HealthCareAbroad\MemcacheBundle\Services\MemcacheService to be return after storing a value to memcached server. This is bound to fail if no Memcache client has been installed.');
     }
@@ -23,7 +23,7 @@ class MemcacheServiceTest extends MemcacheBundleUnitTestCase
      */
     public function testGet()
     {
-        //$returnVal = $this->getServiceContainer()->get('services.memcache')->get($this->key);
+        $returnVal = $this->getServiceContainer()->get('services.memcache')->get($this->key);
 
         $this->assertEquals($this->value, $returnVal, 'Expecting return value of get be equal to the stored value.');
     }
