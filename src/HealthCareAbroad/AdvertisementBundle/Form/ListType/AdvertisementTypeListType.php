c8d7c906570ff556a6ba933f6637b696417ecb5f
Adelbert Silla <adelbert.silla@chromedia.com>
2012-11-26 15:43:40 +0800

    Fixed AdvertisementPropertyValue name. and added AdvertisementTypeListType form.

---
+++ src/HealthCareAbroad/AdvertisementBundle/Form/ListType/AdvertisementTypeListType.php
@@ -0,0 +1,41 @@
+<?php
+namespace HealthCareAbroad\AdvertisementBundle\Form\ListType;
+
+use HealthCareAbroad\AdvertisementBundle\Entity\AdvertisementType;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Doctrine\ORM\EntityRepository;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class AdvertisementTypeListType extends AbstractType 
+{
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'label' => 'Advertisement Type',
+            'property' => 'name',
+            'virtual' => true,
+			'class' => 'HealthCareAbroad\AdvertisementBundle\Entity\AdvertisementType',
+            'query_builder' => function(EntityRepository $er) {
+                return $er->createQueryBuilder('a')->add('where', 'a.status = :status')
+                          ->setParameter('status', AdvertisementType::STATUS_ACTIVE)
+                          ->orderBy('a.name', 'ASC');
+            }
+        ));
+    }
+
+    public function getParent()
+    {
+        return 'entity';
+    }
+
+    public function getName()
+    {
+        return 'advertisementType_list';
+    }
+}
