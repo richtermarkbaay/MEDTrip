8db22443a71404eaa4a25fc02575909ed8853235
Chaztine Blance <chaztine.blance@chromedia.com>
2012-12-03 10:35:53 +0800

    added pager

--- src/HealthCareAbroad/SearchBundle/Services/Admin/DoctorSearchResultBuilder.php
+++ src/HealthCareAbroad/SearchBundle/Services/Admin/DoctorSearchResultBuilder.php
@@ -1,12 +1,21 @@
 <?php
 
 namespace HealthCareAbroad\SearchBundle\Services\Admin;
-
+use Doctrine\Bundle\DoctrineBundle\Registry;
 class DoctorSearchResultBuilder extends SearchResultBuilder
 {
-    private function buildQueryBuilder()
+	protected $doctrine;
+	public function __construct(\Doctrine\Bundle\DoctrineBundle\Registry $doctrine)
+	{
+		$this->doctrine = $doctrine;
+	}
+    protected function buildQueryBuilder($criteria)
     {
-        
+    	
+//     	$this->queryBuilder =  $this->doctrine->getEntityManager()->createQueryBuilder();
+//     	$this->queryBuilder->select('a')->from('DoctorBundle:Doctor', 'a');
+//     	$this->queryBuilder->andWhere('a.firstName LIKE :seachTerm OR a.middleName LIKE :seachTerm OR a.lastName LIKE :seachTerm');
+//     	$this->queryBuilder->setParameter('seachTerm', '%'.$criteria['term'].'%');
     }
     
     protected function buildResult()
