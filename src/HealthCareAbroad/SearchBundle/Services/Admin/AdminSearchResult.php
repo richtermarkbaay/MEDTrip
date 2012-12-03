7063e8763c07057c3df2656a640e9e251cab075f
Alnie Jacobe <alnie.jacobe@chromedia.com>
2012-12-03 13:00:24 +0800

    updated getter and setter

--- src/HealthCareAbroad/SearchBundle/Services/Admin/AdminSearchResult.php
+++ src/HealthCareAbroad/SearchBundle/Services/Admin/AdminSearchResult.php
@@ -4,68 +4,75 @@ namespace HealthCareAbroad\SearchBundle\Services\Admin;
 
 class AdminSearchResult
 {
-	
+    protected $id;
+    protected $firstName;
+    protected $middleName;
+    protected $lastName;
+    protected $name;
+    protected $description;
+    protected $url;
+    
 	public function getId()
 	{
-	
+	    return $this->id;
 	}
-	public function setId()
+	public function setId($id)
 	{
-	
+	    $this->id = $id;
 	}
 	
     public function getName()
     {
-        
+        return $this->name;
     }
     
-    public function setName()
+    public function setName($name)
     {
-        
+        $this->name = $name;
     }
     
     public function getUrl()
     {
-        
+        return $this->url;
     }
     
-    public function setUrl()
+    public function setUrl($url)
     {
-        
+        $this->url = $url;
     }
     
     public function getDescription()
     {
-    	
+    	return $this->description;
     }
-    public function setDescription()
+    public function setDescription($description)
     {
-    
+        $this->description = $description;
     }
     
-    public function getMiddleName()
+    public function getFirstName()
     {
-    
+        return $firstName;
     }
-    public function setMiddleName()
+    public function setFirstName($firstName)
     {
-    
+        $this->firstName = $firstName;
     }
     public function getLastName()
     {
-    
+        return $lastName;
     }
-    public function setLastName()
+    public function setLastName($lastName)
     {
-    
+        $this->lastName = $lastName;
     }
     public function getMiddleName()
     {
-    
+        return $this->middleName;
     }
-    public function setMiddleName()
+    public function setMiddleName($middleName)
     {
-    
+        $this->middleName = $middleName
     }
     public function getFullName()
     {
