09d592c32618ca43d1eb298ebe3bfcf38d9ede96
Allejo Chris Velarde <chris.velarde@chromedia.com>
2012-12-08 23:20:54 +0800

    loaded helperbundle forms.yml

--- src/HealthCareAbroad/HelperBundle/DependencyInjection/HelperExtension.php
+++ src/HealthCareAbroad/HelperBundle/DependencyInjection/HelperExtension.php
@@ -24,6 +24,7 @@ class HelperExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('forms.yml');
         $loader->load('class_labels.yml');
     }
 }
