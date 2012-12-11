45260105ab7e4d299bc3d76e30fdca97a08f604e
Chaztine Blance <chaztine.blance@chromedia.com>
2012-12-11 15:18:12 +0800

    added Manage Medical Provider Group in admin side

---
+++ src/HealthCareAbroad/HelperBundle/Form/MedicalProviderGroupFormType.php
@@ -0,0 +1,33 @@
+<?php
+namespace HealthCareAbroad\HelperBundle\Form;
+
+use HealthCareAbroad\InstitutionBundle\Entity\MedicalProviderGroup;
+
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MedicalProviderGroupFormType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$status = array(MedicalProviderGroup::STATUS_ACTIVE => 'active', MedicalProviderGroup::STATUS_INACTIVE => 'inactive');
+		
+		$builder->add('name', 'text', array('constraints'=>array(new NotBlank())));
+		$builder->add('description', 'text', array('constraints'=>array(new NotBlank())));
+		$builder->add('status', 'choice', array('choices'=>$status));
+	}
+	
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+	{
+		$resolver->setDefaults(array(
+				'data_class' => 'HealthCareAbroad\InstitutionBundle\Entity\MedicalProviderGroup',
+		));
+	}
+
+	public function getName()
+	{
+		return 'medical_provider_group';
+	}
+}
