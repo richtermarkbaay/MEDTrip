e9a07f6db1e81716cff9f49318985560dbece224
Allejo Chris Velarde <chris.velarde@chromedia.com>
2012-12-30 16:40:30 +0800

    Fixes #7. Changed inversedBy orm configuration of GlobalAward

--- src/HealthCareAbroad/HelperBundle/Entity/GlobalAwardTypes.php
+++ src/HealthCareAbroad/HelperBundle/Entity/GlobalAwardTypes.php
@@ -11,9 +11,16 @@ final class GlobalAwardTypes
     
     private static $types = array();
     
+    private static $typeKeys = array();
+    
     public static function getTypes()
     {
         return static::$types;
+    }
+    
+    static public function getTypeKeys()
+    {
+        return static::$typeKeys;
     }
     
     static public function _initTypes()
@@ -22,6 +29,12 @@ final class GlobalAwardTypes
             self::AWARD => 'Award',
             self::CERTIFICATE => 'Certificate',
             self::AFFILIATION => 'Affiliation'
+        );
+        
+        static::$typeKeys = array(
+            GlobalAwardTypes::AWARD => 'award',
+            GlobalAwardTypes::CERTIFICATE => 'certificate',
+            GlobalAwardTypes::AFFILIATION => 'affiliation',
         );
     }
 }
