a20caf7b255ac3472c4d60ab33b9ce4d85b2f13b
Alnie Jacobe <alnie.jacobe@chromedia.com>
2013-01-03 10:13:13 +0800

    Fixes #23,remove duplicate validation messages

--- src/HealthCareAbroad/DoctorBundle/Form/DoctorFormType.php
+++ src/HealthCareAbroad/DoctorBundle/Form/DoctorFormType.php
@@ -21,9 +21,9 @@ class DoctorFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
     	$builder
-     		->add('firstName', 'text', array('label' => 'First name', 'constraints' => array(new NotBlank())))
+     		->add('firstName', 'text', array('label' => 'First name'))
      		->add('middleName', 'text', array('label' => 'Middle name'))
-     		->add('lastName', 'text', array('label' => 'Last name', 'constraints' => array(new NotBlank())))
+     		->add('lastName', 'text', array('label' => 'Last name'))
      		->add('media', 'file', array('label' => 'Image'))
     	    ->add('specializations', new SpecializationListType(), array('expanded' => true,'multiple' => true, 'constraints' => array(new NotBlank())))
     	    ->add('contactEmail', 'text', array('label' => 'Contact Email'))
