bfd0a6d52446661ae7b033727f6956d04ddf1d23
Chaztine Blance <chaztine.blance@chromedia.com>
2013-01-16 17:03:34 +0800

    Fixed the contact number field for doctors

--- src/HealthCareAbroad/HelperBundle/Form/DataTransformer/ContactNumberDataTransformer.php
+++ src/HealthCareAbroad/HelperBundle/Form/DataTransformer/ContactNumberDataTransformer.php
@@ -5,15 +5,24 @@ use Symfony\Component\Form\DataTransformerInterface;
 
 class ContactNumberDataTransformer implements DataTransformerInterface
 {
+    private $defaultValue = array('country_code' => '', 'area_code' => '', 'number' => '');
+    
     public function transform($data)
     {
         $data = \json_decode($data, true);
-        
-        if (!$data) {
-            $data = array('country_code' => '', 'area_code' => '', 'number' => '');
+
+        if (array_key_exists("country_code",$data)){
+            
+           return $data;
         }
-        
-        return $data;
+        if (!$data) {
+            
+           return $data = $this->defaultValue;
+        }else{
+            
+           return $data = $this->defaultValue;
+        }
+     
     }
     
     public function reverseTransform($value)
