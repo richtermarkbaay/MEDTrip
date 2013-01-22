774e1168d91898b3b22840e7d2e914d8b8db2314
Allejo Chris Velarde <chris.velarde@chromedia.com>
2013-01-22 11:40:30 +0800

    Moved ajax requests for tabbed contents of single profile and medical center as part of optimization in #165

--- src/HealthCareAbroad/InstitutionBundle/Controller/UnsecuredController.php
+++ src/HealthCareAbroad/InstitutionBundle/Controller/UnsecuredController.php
@@ -2,6 +2,8 @@
 
 namespace HealthCareAbroad\InstitutionBundle\Controller;
 
+use HealthCareAbroad\InstitutionBundle\Entity\InstitutionMedicalCenter;
+
 use HealthCareAbroad\InstitutionBundle\Form\InstitutionGlobalAwardsSelectorFormType;
 
 use HealthCareAbroad\HelperBundle\Entity\GlobalAwardTypes;
@@ -51,6 +53,11 @@ class UnsecuredController extends Controller
      */
     private $institutionService;
     
+    /**
+     * @var InstitutionMedicalCenter
+     */
+    private $institutionMedicalCenter;
+    
     public function preExecute()
     {
         $this->request = $this->getRequest();
@@ -59,6 +66,16 @@ class UnsecuredController extends Controller
         
         if (!$this->institution) {
             throw $this->createNotFoundException("Invalid institution");
+        }
+        
+        if ($imcId=$this->getRequest()->get('imcId',0)) {
+            $this->institutionMedicalCenter = $this->get('services.institution_medical_center')
+                ->findById($imcId);
+            // institution medical center does not belong to institution
+            if ($this->institutionMedicalCenter && $this->institutionMedicalCenter->getInstitution()->getId() != $this->institution->getId()) {
+                
+                return new Response('Medical center does not belong to institution', 401);
+            }
         }
     }
     
@@ -75,8 +92,8 @@ class UnsecuredController extends Controller
         switch ($content) {
             case 'services':
                 $ancillaryServicesData = array(
-                                'globalList' => $this->get('services.helper.ancillary_service')->getActiveAncillaryServices(),
-                                'selectedAncillaryServices' => array()
+                    'globalList' => $this->get('services.helper.ancillary_service')->getActiveAncillaryServices(),
+                    'selectedAncillaryServices' => array()
                 );
                 
                 foreach ($this->get('services.institution')->getInstitutionServices($this->institution) as $_selectedService) {
@@ -104,10 +121,77 @@ class UnsecuredController extends Controller
         return new Response(\json_encode($output),200, array('content-type' => 'application/json'));
     }
     
-    
+    /**
+     * Load tabbed contents of a medical center
+     * 
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function loadMedicalCenterTabbedContentsAction()
     {
-        return new Response();    
+        if (!$this->institutionMedicalCenter) {
+            throw $this->createNotFoundException("Invalid medical center");
+        }
+        
+        $output = array();
+        $content = $this->request->get('content', null);
+        $institutionMedicalCenterService = $this->get('services.institution_medical_center');
+        $parameters = array('institution' => $this->institution, 'institutionMedicalCenter' => $this->institutionMedicalCenter);
+        $institutionMedicalCenterService = $this->get('services.institution_medical_center');
+        $propertyService = $this->get('services.institution_medical_center_property');
+        switch ($content) {
+            case 'services':
+                $ancillaryServicesData = array(
+                    'globalList' => $this->get('services.helper.ancillary_service')->getActiveAncillaryServices(),
+                    'selectedAncillaryServices' => array()
+                );
+        
+                foreach ($institutionMedicalCenterService->getMedicalCenterServices($this->institutionMedicalCenter) as $_selectedService) {
+                    $ancillaryServicesData['selectedAncillaryServices'][] = $_selectedService->getId();
+                }
+                $parameters['ancillaryServicesData'] = $ancillaryServicesData;
+                $output['services'] = array('html' => $this->renderView('InstitutionBundle:Widgets:tabbedContent.institutionMedicalCenterServices.html.twig', $parameters));
+                break;
+            case 'awards':
+                $form = $this->createForm(new InstitutionGlobalAwardsSelectorFormType());
+                $currentGlobalAwards = $institutionMedicalCenterService->getGroupedMedicalCenterGlobalAwards($this->institutionMedicalCenter);
+                $autocompleteSource = $this->get('services.global_award')->getAutocompleteSource();
+                 
+                $parameters['form'] = $form->createView();
+                $parameters['isSingleCenter'] = true;
+                $parameters['awardsSourceJSON'] = \json_encode($autocompleteSource['award']);
+                $parameters['certificatesSourceJSON'] = \json_encode($autocompleteSource['certificate']);
+                $parameters['affiliationsSourceJSON'] = \json_encode($autocompleteSource['affiliation']);
+                $parameters['currentGlobalAwards'] = $currentGlobalAwards;
+                $parameters['accreditationsSourceJSON'] = \json_encode($autocompleteSource['accreditation']);
+                $output['awards'] = array('html' => $this->renderView('InstitutionBundle:MedicalCenter/Widgets:institutionMedicalCenterAwards.html.twig',$parameters));
+                break;
+            case 'medical_specialists':
+                $doctors = $this->getDoctrine()->getRepository('DoctorBundle:Doctor')->getDoctorsByInstitutionMedicalCenter($this->institutionMedicalCenter->getId());
+                $form = $this->createForm(new \HealthCareAbroad\InstitutionBundle\Form\InstitutionDoctorSearchFormType());
+        
+                if ($this->request->isMethod('POST')) {
+                    $form->bind($this->request);
+        
+                    if ($form->isValid() && $form->get('id')->getData()) {
+                        $center = $this->get('services.institution_medical_center')->saveInstitutionMedicalCenterDoctor($form->getData(), $this->institutionMedicalCenter);
+                        $this->get('session')->setFlash('notice', "Successfully added Medical Specialist");
+                    }
+                }
+                $doctorArr = array();
+        
+                foreach ($doctors as $each) {
+                    $doctorArr[] = array('value' => $each['first_name'] ." ". $each['last_name'], 'id' => $each['id'], 'path' => $this->generateUrl('admin_doctor_load_doctor_specializations', array('doctorId' =>  $each['id'])));
+                }
+        
+                $parameters['form'] = $form->createView();
+                $parameters['doctorsJSON'] = \json_encode($doctorArr, JSON_HEX_APOS);
+                $parameters['institution'] =  $this->institution;
+                $parameters['doctors'] = $this->institutionMedicalCenter->getDoctors();
+                $output['medical_specialists'] = array('html' => $this->renderView('InstitutionBundle:Widgets:tabbedContent.institutionMedicalCenterSpecialists.html.twig',$parameters));
+                break;
+        }
+        
+        return new Response(\json_encode($output),200, array('content-type' => 'application/json'));
     }
     
     /**
