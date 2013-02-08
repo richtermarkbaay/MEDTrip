5aa527255cdda637f77d783062409bd0862b298c
Allejo Chris Velarde <chris.velarde@chromedia.com>
2013-02-08 11:51:22 +0800

    added subspecializations in insert isntitution treatment trigger

--- data/triggers/institution_treatments_triggers.sql
+++ data/triggers/institution_treatments_triggers.sql
@@ -22,8 +22,11 @@ FOR EACH ROW
 BEGIN
     DECLARE DOCUMENT_TYPE_TREATMENT tinyint(3);
     SET DOCUMENT_TYPE_TREATMENT = 3;
+    SET @DOCUMENT_TYPE_SUB_SPECIALIZATION = 2;
+    SET @ACTIVE_IMC_STATUS = 2;
+    SET @ACTIVE_INSTITUTION_STATUS =9; 
     
-    -- insert search terms for this added institution specialization
+    -- insert search terms for this added institution treatment
     INSERT INTO `search_terms` (`term_id`, `institution_id`, `institution_medical_center_id`, `term_document_id`, `document_id`, `type`, `specialization_id`, `sub_specialization_id`, `treatment_id`, `country_id`, `city_id`, `specialization_name`, `sub_specialization_name`, `treatment_name`, `country_name`, `city_name`, `status`)
     SELECT td.`term_id`, inst.`id` as institution_id, imc.`id` as institution_medical_center_id, td.`id` as term_document_id, td.`document_id`, td.`type`,  sp.`id` as specialization_id, NULL as sub_specialization_id, tr.id as treatment_id, inst.`country_id`, inst.`city_id`, sp.`name` as specialization, NULL as sub_specialization, tr.`name` as treatment, co.`name` as country, ci.`name` as city, 1 as status  
     FROM `term_documents` td
@@ -37,8 +40,25 @@ BEGIN
     WHERE ins_sp.`id` = NEW.`institution_specialization_id`
     AND tr.id = NEW.`treatment_id`
     AND imc.status = 2
-    AND inst.status = 9;
-    
+    AND inst.status = 9
+    -- union subspecializations of this added treatment
+    UNION
+    SELECT td.`term_id`, inst.`id` as institution_id, imc.`id` as institution_medical_center_id, td.`id` as term_document_id, td.`document_id`, td.`type`,  sp.`id` as specialization_id, sub_sp.id as sub_specialization_id, tr.id as treatment_id, inst.`country_id`, inst.`city_id`, sp.`name` as specialization, sub_sp.`name` as sub_specialization, tr.`name` as treatment, co.`name` as country, ci.`name` as city, 1 as status
+    FROM `term_documents` td
+    INNER JOIN `sub_specializations` sub_sp ON td.`document_id` = sub_sp.`id` AND td.`type` = @DOCUMENT_TYPE_SUB_SPECIALIZATION
+    INNER JOIN `treatment_sub_specializations` tr_sub_sp ON sub_sp.`id` = tr_sub_sp.`sub_specialization_id`
+    INNER JOIN `treatments` tr ON  tr_sub_sp.`treatment_id` = tr.`id`
+    INNER JOIN `specializations` sp ON tr.`specialization_id` = sp.`id`
+    INNER JOIN `institution_specializations` ins_sp ON sp.`id` = ins_sp.`specialization_id`
+    INNER JOIN `institution_medical_centers` imc ON imc.`id` = ins_sp.`institution_medical_center_id`
+    INNER JOIN `institutions` inst ON inst.`id` = imc.`institution_id`
+    INNER JOIN `countries` co ON co.`id` = inst.`country_id`
+    LEFT JOIN `cities` ci ON ci.id = inst.`country_id`
+    WHERE 1
+    AND ins_sp.`id` = NEW.`institution_specialization_id`
+    AND tr.id = NEW.`treatment_id`
+    AND imc.status = @ACTIVE_IMC_STATUS
+    AND inst.status = @ACTIVE_INSTITUTION_STATUS;
 END; $$
 ### end institution_treatments_ai trigger definition
 
