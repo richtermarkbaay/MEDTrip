9409703619a7332857216e76af593f0b749f5f86
Chaztine Blance <chaztine.blance@chromedia.com>
2013-02-19 14:55:00 +0800

    Added InstitutionSpecializationControllerTest.php

---
+++ src/HealthCareAbroad/AdminBundle/Tests/Controller/InstitutionSpecializationControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author Chaztine Blance
+ */
+
+namespace HealthCareAbroad\AdminBundle\Tests\Controller;
+
+use HealthCareAbroad\AdminBundle\Tests\AdminBundleWebTestCase;
+
+class InstitutionSpecializationControllerTest extends AdminBundleWebTestCase
+{
+    
+    public function testInvalidTreatmentId(){
+        // test invalid treatmend Id
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $uri = '/admin/institution/1/institutionSpecialization/1/ajaxRemoveSpecializationTreatment/343434';
+        $client->request('POST', $uri);
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+    
+    public function testAjaxRemoveSpecializationTreatment(){
+        
+        $uri = '/admin/institution/1/institutionSpecialization/1/ajaxRemoveSpecializationTreatment/1';
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $crawler = $client->request('POST', $uri);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $uri = '/admin/institution/1/institutionSpecialization/1323/ajaxRemoveSpecializationTreatment/2';
+        $client->request('POST', $uri);
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+    
+    public function testajaxViewMedicalSpecializationTreatments(){
+        
+        $uri = '/admin/institution/1/medical-center/1/specializations/1/ajaxEditInstitutionSpecialization';
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $crawler = $client->request('GET', $uri);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        
+        
+        $invalidUri = '/admin/institution/2323/medical-center/2323/specializations/1/ajaxEditInstitutionSpecialization';
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $crawler = $client->request('GET', $invalidUri);
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+    
+}
