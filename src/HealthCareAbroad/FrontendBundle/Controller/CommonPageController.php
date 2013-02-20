a60d7e5034dce74ae6e377ed7eb83d5dbcc2e1a9
Alnie Jacobe <alnie.jacobe@chromedia.com>
2013-02-20 17:30:20 +0800

    Fixes #347

--- src/HealthCareAbroad/FrontendBundle/Controller/CommonPageController.php
+++ src/HealthCareAbroad/FrontendBundle/Controller/CommonPageController.php
@@ -2,6 +2,10 @@
 
 namespace HealthCareAbroad\FrontendBundle\Controller;
 
+use HealthCareAbroad\AdminBundle\Entity\Inquiry;
+
+use HealthCareAbroad\FrontendBundle\Form\InquiryType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
@@ -17,5 +21,38 @@ class CommonPageController extends Controller
     public function viewTermsOfUseAction(){
     
         return $this->render('FrontendBundle:Static:termsOfUse.html.twig');
+    }
+    
+    public function saveInquiryAction(Request $request)
+    {
+        $inquiry = new Inquiry();
+        $form = $this->createForm(new InquiryType(), $inquiry);
+        $inquirySubjects = $this->getDoctrine()->getRepository('AdminBundle:InquirySubject')->findAll();
+        
+        if($request->isMethod('POST')) {
+            $form->bind($request);
+            if($form->isValid()) {
+                
+                //get IP Address
+                $remoteAddress = $this->getRequest()->getClientIp();
+                
+                $inquirySubject = $this->getDoctrine()->getRepository('AdminBundle:InquirySubject')->findOneByName($request->get('inquirySubject'));
+                $inquiry->setInquirySubject($inquirySubject);
+                $inquiry->setRemoteAddress($remoteAddress);
+                $inquiry->setStatus(Inquiry::STATUS_ACTIVE);
+                
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($inquiry);
+                $em->flush();
+                
+                $request->getSession()->setFlash('success', 'Inquiry has been send.');
+    
+            }
+        }
+        
+        return $this->render('FrontendBundle:Static:inquiry.html.twig', 
+                        array('form' => $form->createView(),
+                              'inquirySubjects' => $inquirySubjects,
+                              'isInquiry' => 1));
     }
 }
