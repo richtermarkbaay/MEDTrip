2eab587d1a73fd779a3c1fa6eb94065ccbb60655
Alnie Jacobe <alnie.jacobe@chromedia.com>
2013-02-20 09:07:39 +0800

    added functional tests

--- src/HealthCareAbroad/AdminBundle/Tests/Controller/InstitutionTreatmentsControllerTest.php
+++ src/HealthCareAbroad/AdminBundle/Tests/Controller/InstitutionTreatmentsControllerTest.php
@@ -35,22 +35,126 @@ class InstitutionTreatmentsControllerTest extends AdminBundleWebTestCase
 //         $this->assertGreaterThan(0, $crawler->filter('html:contains("' .$institution->getName(). ' Medical Centers")')->count(), '"Add Treatment" string not found!');
         
 //     }
+//     public function testEditMedicalCenterAction()
+//     {
+//         $client = $this->getBrowserWithActualLoggedInUser();
+//         $crawler = $client->request('GET', '/admin/institution/1/medical-center/1/edit');
     
+//         $validValues = array(
+//                         'institutionMedicalCenter[name]' => 'asd anem',
+//                         'institutionMedicalCenter[description]' => 'sad description',
+//                         'institutionMedicalCenter[contactEmail]' => '',
+//                         'institutionMedicalCenter[contactNumber][country_code]' => '',
+//                         'institutionMedicalCenter[address][room_number]' => '',
+//                         'institutionMedicalCenter[address][building]' => '',
+//                         'institutionMedicalCenter[address][street]' => '',
+//                         'institutionMedicalCenter[websites][main]' => '',
+//                         'institutionMedicalCenter[websites][facebook]' => '',
+//                         'institutionMedicalCenter[websites][twitter]' => '',
+//         );
+//         $form = $crawler->selectButton('submit')->form();
+//         $crawler = $client->submit($form, $validValues);
+//         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+//     }
     public function testViewAllMedicalCentersAction()
     {
         $client = $this->getBrowserWithActualLoggedInUser();
         $crawler = $client->request('GET', '/admin/institution/1/medical-centers');
     
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        
+        $crawler = $client->request('GET', '/admin/institution/2/medical-centers');
+        
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        
     }
-    
     public function testViewMedicalCenterAction()
     {
         $client = $this->getBrowserWithActualLoggedInUser();
         $crawler = $client->request('GET', '/admin/institution/1/medical-center/view/1');
-        
+    
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+    public function testAddMedicalCenterAction()
+    {
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $crawler = $client->request('GET', '/admin/institution/1/medical-center/add');
+        $validValues = array(
+                        'institutionMedicalCenter[name]' => 'asd anem',
+                        'institutionMedicalCenter[description]' => 'sad description',
+                        'institutionMedicalCenter[contactEmail]' => '',
+                        'institutionMedicalCenter[contactNumber][country_code]' => '',
+                        'institutionMedicalCenter[address][room_number]' => '',
+                        'institutionMedicalCenter[address][building]' => '',
+                        'institutionMedicalCenter[address][street]' => '',
+                        'institutionMedicalCenter[websites][main]' => '',
+                        'institutionMedicalCenter[websites][facebook]' => '',
+                        'institutionMedicalCenter[websites][twitter]' => '',
+                        'institutionMedicalCenter[city]' => 'palakad',
+                        'institutionMedicalCenter[zipCode]' => '678001',
+                        'institutionMedicalCenter[state]' => 'kerala',
+                        'institutionMedicalCenter[country]' => 'india',
+                        'institutionMedicalCenter[status]' => '1',
+                        'institutionMedicalCenter[timeZone]' => '',
+                        'institutionMedicalCenter[businessHours]' => ''
+        );
+        $invalidValues = array();
+        
+        $form = $crawler->selectButton('submit')->form();
+        $crawler = $client->submit($form, $invalidValues);
+        //$this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Center name is required.")')->count());
+        
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $crawler = $client->request('GET', '/admin/institution/1/medical-center/add');
+        $form = $crawler->selectButton('submit')->form();
+        $crawler = $client->submit($form, $validValues);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+    
+    public function testAddGlobalAwardsAction()
+    {
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $crawler = $client->request('GET', '/admin/institution/1/medical-center/1/global_awards');
+        
+        $this->assertEquals(500, $client->getResponse()->getStatusCode());
+    }
+    
+    public function testUpdateGlobalAwardsAction()
+    {
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $crawler = $client->request('GET', '/admin/institution/1/medical-center/1/global_awards/1');
+    
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+    
+    
+    public function testAddInstitutionTreatmentsAction()
+    {
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $crawler = $client->request('GET', '/admin/institution/1/medical-center/1/global_awards/1');
+        
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+    public function testEditMedicalCenterStatusAction()
+    {
+        $client = $this->getBrowserWithActualLoggedInUser();
+        $invalidStatus = 35;
+        $crawler = $client->request('POST', '/admin/institution/1/medical-center/1/edit-status', array('status' => $invalidStatus));
+
+        $response = $client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        
+        $crawler = $client->request('GET', '/admin/institution/1/medical-center/1/edit-status');
+        $response = $client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+//         $crawler = $client->request('GET', '/admin/institution/1/medical-center/1/edit-status');
+//         $validValues = array('institutionMedicalCenter[status]' => '2');
+//         $form = $crawler->selectButton('submit')->form();
+//         $crawler = $client->submit($form, $validValues);
+//         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+   }
+    
     
 //     public function testAddMedicalCenterDetailsAction()
 //     {
