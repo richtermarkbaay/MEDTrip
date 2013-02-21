431eeda0337b4694e53b03908f652a2906ea5b4b
Chaztine Blance <chaztine.blance@chromedia.com>
2013-02-21 10:42:53 +0800

    Fixes #346

--- src/HealthCareAbroad/FrontendBundle/Resources/public/js/Feedback.js
+++ src/HealthCareAbroad/FrontendBundle/Resources/public/js/Feedback.js
@@ -1,40 +1,111 @@
-var Feedback = {
-    submitFeedbackMessageForm: function (_domButtonElement) {
-        _button = $(_domButtonElement);
-        _buttonHtml = _button.html();
-        _button.html("Processing...").attr('disabled', true);
-        _form = _button.parents('.modal').find('form');
-        _fieldParent = $(_button).attr('data-divParent');
-        _data = _form.serialize();
+var Feedback = {  
+    feedbackComponents: {},
+    
+    feedbackFormInputIdPrefix: 'feedbackMessage',
+    
+    restoreInitialState: function() {
+        Feedback.clearErrors().resetForm().resetAlertBox();
+    },
+    
+    resetForm: function() {
+        this.feedbackComponents.form.show().find('input[type="text"],input[type="emailAddress"], textarea').val('');
+        this.feedbackComponents.submitButton.attr('disabled', false).show();
+        return this;
+    },
+    
+    clearErrors: function() {
+    	Feedback.feedbackComponents.form.find('.error').removeClass('error');
+        return this;
+    },
+    
+    resetAlertBox: function(){
+        Feedback.feedbackComponents.modal.find('.alert-box').removeClass('alert alert-error alert-success').html("");
+        return this;
+    },
+    
+    showAlertError: function() {
+        Feedback
+            .resetAlertBox()
+            .feedbackComponents.modal.find('.alert-box')
+            .addClass('alert alert-error')
+            .html('Please fill up the form properly.');
+        return this;
+    },
+    
+    showAlertSuccess: function() {
+        Feedback
+        .resetAlertBox()
+        .feedbackComponents.modal.find('.alert-box')
+        .addClass('alert alert-success')
+        .html('Message sent.');
+        return this;
+    },
+    
+    submitFeedbackMessageForm: function(){
+        Feedback.clearErrors();
         $.ajax({
-            url: _form.attr('action'),
-            data: _data,
+            url: Feedback.feedbackComponents.form.attr('action'),
+            data: Feedback.feedbackComponents.form.serialize(),
             type: 'POST',
             dataType: 'json',
-            success: function(response) {
-            	 _form.parents('.modal').modal('hide');
-                 _button.html("Send Feedback").attr('disabled', false);
+            success: function(response){
+                Feedback.feedbackComponents.submitButton
+                .html(Feedback.feedbackComponents.submitButton.attr('data-html'))
+                .attr('disabled', false)
+                .hide();
+                Feedback.feedbackComponents.form.hide();
+                Feedback.showAlertSuccess();
             },
-            error: function(json) {
-            	Feedback.removeErrors("#feedbackForm");
-                _button.html(_buttonHtml).attr('disabled', false);
-                if (json.status == 400) {
-                    // invalid form
-                	_json = $.parseJSON(json.responseText);
-                	$.each(_json.html, function(key, item){
-                		_field = _fieldParent+item.field;
-                		$('#feedbackForm').find(_field).addClass('error');
-                	});
-                	$('div.alert-error').addClass('alert').append("Please fill up the form properly.");
+            error: function(response){
+                Feedback.feedbackComponents.submitButton
+                .html(Feedback.feedbackComponents.submitButton.attr('data-html'))
+                .attr('disabled', false);
+                if (response.status==400) {
+                    var errors = $.parseJSON(response.responseText).html;
+                    if (errors.length) {
+                        Feedback.showAlertError();
+                        $.each(errors, function(key, item){
+                            $('#'+Feedback.feedbackFormInputIdPrefix+'_'+item.field).addClass('error');
+                        });
+                    }
                 }
-                
             }
         });
-    },
-	removeErrors: function(_formId) {
-		_formId = $(_formId);
-		_formId.find('.control-group').removeClass('error');
-		_formId.find('.error').removeClass('error');
-		$('div.alert-error').removeClass('alert').html('');
-	},
+    }
+    
 };
+
+
+(function($){
+    $.fn.feedbackModalForm = function(_components){
+        
+        Feedback.feedbackComponents = {
+            'modal': this,
+            'submitButton': _components.submitButton,
+            'form': _components.form
+        };
+        
+        Feedback.feedbackComponents.modal.live('show', function(){
+            Feedback.restoreInitialState();
+        });
+        
+        // initialize submit submitButton
+        Feedback.feedbackComponents.submitButton.click(function(){
+            $(this).attr('data-html', $(this).html())
+                .html($(this).attr('data-loader-text'))
+                .attr('disabled', true);
+            
+            Feedback.feedbackComponents.form.submit();
+            
+            return false;
+        });
+        Feedback.feedbackComponents.form.submit(function(){
+            
+            Feedback.submitFeedbackMessageForm();
+            
+            return false;
+        });
+        
+        return this;
+    }
+})(jQuery);
