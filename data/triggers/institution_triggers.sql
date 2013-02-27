83b12754f43e19a858695942f7c22351131fac24
Harold Modesto <harold.modesto@chromedia.com>
2013-02-27 09:01:50 +0800

    added country column in institution trigger

--- data/triggers/institution_triggers.sql
+++ data/triggers/institution_triggers.sql
@@ -21,6 +21,7 @@ CREATE TRIGGER institutions_au AFTER UPDATE on `institutions`
 FOR EACH ROW
 BEGIN
     DECLARE _city_name VARCHAR(250);
+    DECLARE _country_name VARCHAR(250);
     SET @ACTIVE_INSTITUTION_STATUS = 9;
     SET @ACTIVE_SEARCH_TERM_STATUS = 1;
     SET @INACTIVE_SEARCH_TERM_STATUS = 0;
@@ -38,6 +39,11 @@ BEGIN
         UPDATE `search_terms` SET `status` = @INACTIVE_SEARCH_TERM_STATUS
         WHERE `institution_id` = OLD.`id`;
     END IF;
+
+    IF OLD.country_id != NEW.country_id THEN
+        SELECT name INTO _country_name FROM countries WHERE id = NEW.country_id;
+        UPDATE search_terms SET country_id = NEW.country_id, country_name = _country_name WHERE institution_id = OLD.id;
+    ENDIF;
 
     IF OLD.city_id IS NOT NULL AND NEW.city_id IS NULL THEN
         UPDATE search_terms SET city_id = NULL, city_name = NULL WHERE institution_id = OLD.id;
