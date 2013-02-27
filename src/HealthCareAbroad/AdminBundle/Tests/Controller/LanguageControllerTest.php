13dcdcc2260dee19b77895f7ca9459334d051a72
Alnie Jacobe <alnie.jacobe@chromedia.com>
2013-02-27 16:06:32 +0800

    completed test

--- src/HealthCareAbroad/AdminBundle/Tests/Controller/LanguageControllerTest.php
+++ src/HealthCareAbroad/AdminBundle/Tests/Controller/LanguageControllerTest.php
@@ -20,120 +20,121 @@ class LanguageControllerTest extends AdminBundleWebTestCase
         $this->assertEquals(403, $client->getResponse()->getStatusCode());
     }
     
-//     public function testAdd()
-//     {
-//     	$client = $this->getBrowserWithActualLoggedInUser();
-//     	$crawler = $client->request('GET', '/admin/language/add');
+    public function testAdd()
+    {
+    	$client = $this->getBrowserWithActualLoggedInUser();
+    	$crawler = $client->request('GET', '/admin/language/add');
     
-//     	$this->assertEquals(200, $client->getResponse()->getStatusCode());
-//     	$this->assertGreaterThan(0, $crawler->filter('html:contains("Name")')->count(), '"Name" string not found!');
-//     }
-//     public function testEdit()
-//     {
-//     	$client = $this->getBrowserWithActualLoggedInUser();
-//     	$crawler = $client->request('GET', '/admin/language/edit/1');
+    	$this->assertEquals(200, $client->getResponse()->getStatusCode());
+    	$this->assertGreaterThan(0, $crawler->filter('html:contains("Name")')->count(), '"Name" string not found!');
+    }
     
-//     	$this->assertEquals(200, $client->getResponse()->getStatusCode());
-//     	$this->assertGreaterThan(0, $crawler->filter('html:contains("Name")')->count(), '"Name" string not found!');
-//     }
+    public function testEdit()
+    {
+    	$client = $this->getBrowserWithActualLoggedInUser();
+    	$crawler = $client->request('GET', '/admin/language/edit/1');
     
-//     public function testSave()
-//     {
-//     	$client = $this->getBrowserWithActualLoggedInUser();
-//     	$crawler = $client->request('GET', '/admin/language/add');
+    	$this->assertEquals(200, $client->getResponse()->getStatusCode());
+    	$this->assertGreaterThan(0, $crawler->filter('html:contains("Name")')->count(), '"Name" string not found!');
+    }
     
-//     	$formData = array(
-//     					'language[name]' => 'TestLanguage1',
-//     					'language[status]' => 1
-//     	);
+    public function testSave()
+    {
+    	$client = $this->getBrowserWithActualLoggedInUser();
+    	$crawler = $client->request('GET', '/admin/language/add');
     
-//     	$form = $crawler->selectButton('submit')->form();
-//     	$crawler = $client->submit($form, $formData);
+    	$formData = array(
+    					'language[name]' => 'TestLanguage1',
+    					'language[status]' => 1
+    	);
     
-//     }
+    	$form = $crawler->selectButton('submit')->form();
+    	$crawler = $client->submit($form, $formData);
+    
+    }
 
-//     public function testEditSave()
-//     {
-//     	$client = $this->getBrowserWithActualLoggedInUser();
-//     	$crawler = $client->request('GET', '/admin/language/edit/1');
+    public function testEditSave()
+    {
+    	$client = $this->getBrowserWithActualLoggedInUser();
+    	$crawler = $client->request('GET', '/admin/language/edit/1');
     
-//     	$formData = array(
-//     					'language[name]' => 'TestLanguage1 Updated',
-//     					'language[status]' => 1
-//     	);
+    	$formData = array(
+    					'language[name]' => 'TestLanguage1 Updated',
+    					'language[status]' => 1
+    	);
     
-//     	$form = $crawler->selectButton('submit')->form();
-//     	$crawler = $client->submit($form, $formData);
+    	$form = $crawler->selectButton('submit')->form();
+    	$crawler = $client->submit($form, $formData);
     
-//     	// check if redirect code 302
-//     	$this->assertEquals(302, $client->getResponse()->getStatusCode());
+    	// check if redirect code 302
+    	$this->assertEquals(302, $client->getResponse()->getStatusCode());
     
-//     	// check of redirect url /admin/language
-//     	$this->assertEquals('/admin/language', $client->getResponse()->headers->get('location'));
-    
-    
-//     	// redirect request
-//     	$crawler = $client->followRedirect(true);
-    
-//     	// check if the redirected response content has the newly added language name
-//     	$isAdded = $crawler->filter('html:contains("Name")')->count() > 0;
-//     	$this->assertTrue($isAdded);
-//     }
-    
-//     public function testCreateDuplicate()
-//     {
-//     	$client = $this->getBrowserWithActualLoggedInUser();
-//     	$crawler = $client->request('GET', '/admin/language/add');
-    
-//     	$formData = array(
-//     					'language[name]' => 'test',
-//     					'language[status]' => 1
-//     	);
-    
-//     	$form = $crawler->selectButton('submit')->form();
-//     	$crawler = $client->submit($form, $formData);
-    
-//     	// check if status code is not 302
-//     	$this->assertNotEquals(302, $client->getResponse()->getStatusCode(), '"Language" must not be able to create an entry with duplicate name.');
-//     }
+    	// check of redirect url /admin/language
+    	$this->assertEquals('/admin/language', $client->getResponse()->headers->get('location'));
     
     
-//     public function testUpdateStatusAction(){
-//     	$client = $this->getBrowserWithActualLoggedInUser();
-//     	$crawler = $client->request('GET', '/admin/language/update-status/1');
+    	// redirect request
+    	$crawler = $client->followRedirect(true);
     
-//     	$response = $client->getResponse();
-//     	$this->assertEquals("Response code: 200", "Response code: " . $response->getStatusCode());
-//     }
+    	// check if the redirected response content has the newly added language name
+    	$isAdded = $crawler->filter('html:contains("Name")')->count() > 0;
+    	$this->assertTrue($isAdded);
+    }
     
-//     public function testSaveInvalidData()
-//     {
-//     	$client = $this->getBrowserWithActualLoggedInUser();
-//     	$crawler = $client->request('GET', '/admin/language/add');
+    public function testCreateDuplicate()
+    {
+    	$client = $this->getBrowserWithActualLoggedInUser();
+    	$crawler = $client->request('GET', '/admin/language/add');
     
-//     	$formData = array(
-//     					'language[name]' => ' ',
-//     					'language[status]' => 1
-//     	);
+    	$formData = array(
+    					'language[name]' => 'test',
+    					'language[status]' => 1
+    	);
     
-//     	$form = $crawler->selectButton('submit')->form();
-//     	$crawler = $client->submit($form, $formData);
+    	$form = $crawler->selectButton('submit')->form();
+    	$crawler = $client->submit($form, $formData);
     
-//     	$this->assertEquals(200, $client->getResponse()->getStatusCode(), 'Invalid data has been created!');
-//     	$this->assertGreaterThan(0, $crawler->filter('html:contains("Name")')->count(), 'No validation message!');
-//     }
+    	// check if status code is not 302
+    	$this->assertNotEquals(302, $client->getResponse()->getStatusCode(), '"Language" must not be able to create an entry with duplicate name.');
+    }
     
-//     public function testSaveInvalidMethod()
-//     {
-//     	$client = $this->getBrowserWithActualLoggedInUser();
     
-//     	$formData = array(
-//     					'language[name]' => 'saveUsingGet',
-//     					'language[status]' => 1
-//     	);
-//     	$crawler = $client->request('GET', '/admin/language/test-save', $formData);
-//     	$this->assertEquals(405, $client->getResponse()->getStatusCode(), 'Invalid method accepted!');
-//     }
+    public function testUpdateStatusAction(){
+    	$client = $this->getBrowserWithActualLoggedInUser();
+    	$crawler = $client->request('GET', '/admin/language/update-status/1');
+    
+    	$response = $client->getResponse();
+    	$this->assertEquals("Response code: 200", "Response code: " . $response->getStatusCode());
+    }
+    
+    public function testSaveInvalidData()
+    {
+    	$client = $this->getBrowserWithActualLoggedInUser();
+    	$crawler = $client->request('GET', '/admin/language/add');
+    
+    	$formData = array(
+    					'language[name]' => ' ',
+    					'language[status]' => 1
+    	);
+    
+    	$form = $crawler->selectButton('submit')->form();
+    	$crawler = $client->submit($form, $formData);
+    
+    	$this->assertEquals(200, $client->getResponse()->getStatusCode(), 'Invalid data has been created!');
+    	$this->assertGreaterThan(0, $crawler->filter('html:contains("Name")')->count(), 'No validation message!');
+    }
+    
+    public function testSaveInvalidMethod()
+    {
+    	$client = $this->getBrowserWithActualLoggedInUser();
+    
+    	$formData = array(
+    					'language[name]' => 'saveUsingGet',
+    					'language[status]' => 1
+    	);
+    	$crawler = $client->request('GET', '/admin/language/test-save', $formData);
+    	$this->assertEquals(405, $client->getResponse()->getStatusCode(), 'Invalid method accepted!');
+    }
     
    
 }
