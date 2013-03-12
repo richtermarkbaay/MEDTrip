cde768d3155dec1ee4691335c0faaf71c78fe7f7
Harold Modesto <harold.modesto@chromedia.com>
2013-03-12 10:56:32 +0800

    added more exempted routes

--- src/HealthCareAbroad/FrontendBundle/Listener/DefaultRouterListener.php
+++ src/HealthCareAbroad/FrontendBundle/Listener/DefaultRouterListener.php
@@ -26,14 +26,17 @@ class DefaultRouterListener
         $request = $event->getRequest();
 
         if (null === $request->attributes->get('_route')){
-
             $pathInfo = $request->getPathInfo();
 
             // check first if it is /admin/ or /institution/, do nothing if it matches since it should be forwarded to the router
             // this check is necessary if this listner's priority is higher than RouterListener priority to save execution time
-            if ((strpos($pathInfo, '/admin') !== false) || (strpos($pathInfo, '/institution') !== false) || (strpos($pathInfo, '/search') !== false)) {
+//             if ((strpos($pathInfo, '/admin') !== false) || (strpos($pathInfo, '/institution') !== false) || (strpos($pathInfo, '/frontend_search') !== false)) {
+//                 return;
+//             }
+            if ($this->isConfiguredRoute($pathInfo)) {
                 return;
             }
+
 
             $routeObj = null;
 
@@ -50,9 +53,26 @@ class DefaultRouterListener
             $request->attributes->set('_route_params', $variables);
 
             // TODO - This route does not exists! should be change when error occur!
-            // Added by: Adelbert Silla 
+            // Added by: Adelbert Silla
             // Being used in breadcrumbs for combined search.
             $request->attributes->set('_route', 'frontend_search_combined');
         }
+    }
+
+    private function isConfiguredRoute($pathInfo)
+    {
+        if (
+            strpos($pathInfo, '/admin') !== false ||
+            strpos($pathInfo, '/institution') !== false ||
+            strpos($pathInfo, '/frontend_search') !== false ||
+            strpos($pathInfo, '/hospital') !== false ||
+            strpos($pathInfo, '/index.html') !== false ||
+            strpos($pathInfo, '/destinations.html') !== false ||
+            strpos($pathInfo, '/treatments.html') !== false
+        ) {
+            return true;
+        }
+
+        return false;
     }
 }
