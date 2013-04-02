9a245c6c1d9c648ec53ed9f30752c0860b1a307d
Harold Modesto <harold.modesto@chromedia.com>
2013-04-02 18:42:47 +0800

    added option to use only a single number and a prefix to the label

--- src/HealthCareAbroad/HelperBundle/Form/FieldType/ContactNumberWithFlagFieldType.php
+++ src/HealthCareAbroad/HelperBundle/Form/FieldType/ContactNumberWithFlagFieldType.php
@@ -2,6 +2,12 @@
 
 namespace HealthCareAbroad\HelperBundle\Form\FieldType;
 
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Symfony\Component\Form\FormInterface;
+
+use Symfony\Component\Form\FormView;
+
 use HealthCareAbroad\HelperBundle\Form\DataTransformer\ContactNumberWithWidgetDataTransformer;
 
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,12 +20,23 @@ class ContactNumberWithFlagFieldType extends AbstractType
     {
         $builder->addModelTransformer(new ContactNumberWithWidgetDataTransformer());
     }
-    
+
     public function getName()
     {
         return 'contact_number_with_flag';
     }
-    
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array('label_prefix' => '', 'display_both' => true));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['label_prefix'] = $options['label_prefix'];
+        $view->vars['display_both'] = $options['display_both'];
+    }
+
     public function getParent()
     {
         return 'text';
