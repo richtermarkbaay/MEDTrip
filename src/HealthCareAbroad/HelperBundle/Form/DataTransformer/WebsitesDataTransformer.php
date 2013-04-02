1a0f3875844c991943f945eb1079e72c160ce44e
Harold Modesto <harold.modesto@chromedia.com>
2013-04-02 18:59:11 +0800

    added google plus to defaults

--- src/HealthCareAbroad/HelperBundle/Form/DataTransformer/WebsitesDataTransformer.php
+++ src/HealthCareAbroad/HelperBundle/Form/DataTransformer/WebsitesDataTransformer.php
@@ -5,14 +5,14 @@ use Symfony\Component\Form\DataTransformerInterface;
 
 class WebsitesDataTransformer implements  DataTransformerInterface
 {
-    private $defaultValue = array('main' => '', 'facebook' => '','twitter' => '');
+    private $defaultValue = array('main' => '', 'facebook' => '','twitter' => '', 'googleplus' => '');
     public function transform($value)
     {
-        
+
         $jsonValue = \json_decode($value, true);
-        
+
         if (!$jsonValue) {
-            
+
             $jsonValue = $this->defaultValue;
         }
         else {
@@ -23,16 +23,16 @@ class WebsitesDataTransformer implements  DataTransformerInterface
                 }
             });
         }
-        
+
         return $jsonValue;
     }
-    
+
     public function reverseTransform($value)
     {
         if (\is_null($value)) {
             $value = $this->defaultValue;
         }
-        
+
         if (!is_array($value)) {
             throw new \Exception(__CLASS__.' expects $value to be an array, '.\gettype($value).' given');
         }
