831a177dcf44c0526992e39f07c48dbf0d6585f5
Chaztine Blance <chaztine.blance@chromedia.com>
2013-04-02 16:29:04 +0800

    changed event.keyCode to event.ctrlKey

--- src/HealthCareAbroad/HelperBundle/Resources/public/js/Flag.js
+++ src/HealthCareAbroad/HelperBundle/Resources/public/js/Flag.js
@@ -89,13 +89,13 @@
     	 }).bind( "keydown", function( event ) {
     		 if (!(event.keyCode == 8                                // backspace
                    || event.keyCode == 9                               // tab
-                   || event.keyCode == 17                              // ctrl
+                   || event.ctrlKey == 17                              // ctrl
                    || event.keyCode == 46                              // delete
                    || event.keyCode == 61								// +
                    || (event.keyCode >= 35 && event.keyCode <= 40)     // arrow keys/home/end
                    || (event.keyCode >= 48 && event.keyCode <= 57)     // numbers on keyboard
                    || (event.keyCode >= 96 && event.keyCode <= 105)    // number on keypad
-                   || (event.keyCode == 65 && prevKey == 17 && prevControl == event.currentTarget.id))          // ctrl + a, on same control
+                   || (event.keyCode == 65 && event.ctrlKey == 17 && prevControl == event.currentTarget.id))          // ctrl + a, on same control
                ) {
 				event.preventDefault();					
 			}			
