105437278698270b9da51b721f0fc0056bf2da21
Chaztine Blance <chaztine.blance@chromedia.com>
2013-05-22 14:06:38 +0800

    added the hca_intro draft #495

---
+++ src/HealthCareAbroad/FrontendBundle/Controller/CampaignIntroController.php
@@ -0,0 +1,15 @@
+<?php 
+namespace HealthCareAbroad\FrontendBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CampaignIntroController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        return $this->render('FrontendBundle:CampaignIntro:index.html.twig');
+    }
+}
+?>
