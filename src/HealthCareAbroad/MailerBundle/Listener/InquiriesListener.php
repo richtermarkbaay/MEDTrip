7b86cc6ac0ae0a9d60126d606bb8f8a59d6c345d
Harold Modesto <harold.modesto@chromedia.com>
2013-07-10 16:02:50 +0800

    added inquiries email notifications

--- src/HealthCareAbroad/MailerBundle/Listener/InquiriesListener.php
+++ src/HealthCareAbroad/MailerBundle/Listener/InquiriesListener.php
@@ -11,23 +11,52 @@ class InquiriesListener extends NotificationsListener
     public function getData(Event $event)
     {
         $inquiry = $event->getSubject();
-        $institution = $event->getArgument('institution');
+        $institution = $inquiry->getInstitution();
+        $institutionMedicalCenter = $inquiry->getInstitutionMedicalCenter();
+
         $router = $this->container->get('router');
 
-        $message = <<< EOT
-{$inquiry->getInquirySubject()->getName()}
-{$inquiry->getMessage()}
-EOT;
+        if ($institutionMedicalCenter) {
+            $facilityName = $institutionMedicalCenter->getName();
+            $urlFacility = $router->generate('frontend_institutionMedicaCenter_profile', array(
+                            'institutionSlug' => $institution->getSlug(),
+                            'imcSlug' => $institutionMedicalCenter->getSlug()),
+                            true
+            );
+
+            $to = $institutionMedicalCenter->getContactEmail();
+
+        } else {
+            $facilityName = $institution->getName();
+
+            switch ($institution->getType()) {
+                case InstitutionTypes::SINGLE_CENTER:
+                    $routeName = 'frontend_single_center_institution_profile';
+                    break;
+                case InstitutionTypes::MULTIPLE_CENTER:
+                    $routeName = 'frontend_multiple_center_institution_profile';
+                    break;
+            }
+
+            $urlFacility = $router->generate($routeName, array('institutionSlug' => $institution->getSlug()), true);
+        }
+
+        if (empty($to)) {
+            if (!$to = $institution->getContactEmail()) {
+                $to = $this->container->get('services.institution')->getAccountOwner($institution)->getEmail();
+            }
+        }
+
         return array(
             'inquiry' => array(
-                'name' => $inquiry->getFirstName().' '.$inquiry->getLastName(),
-                'email' => $inquiry->getEmail(),
-                'message' => $message,
+                'name' => $inquiry->getInquirerName(),
+                'email' => $inquiry->getInquirerEmail(),
+                'message' => $inquiry->getMessage(),
             ),
-            'to' => 'inquiry@healthcareabroad.com',
-            'institution_name' => $institution->getName(),
+            'to' => $to,
+            'facility_name' => $facilityName,
             'url' => array(
-                'institution' => $router->generate('institution_account_profile', array(), true)
+                'facility' => $urlFacility
             )
         );
     }
