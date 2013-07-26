b950ab3766d2caf69e462f9f0dd25b7fb109aeef
Adelbert Silla <adelbert.silla@chromedia.com>
2013-07-26 18:10:43 +0800

    Fixes issue #552.

--- src/HealthCareAbroad/AdminBundle/Form/InstitutionFormType.php
+++ src/HealthCareAbroad/AdminBundle/Form/InstitutionFormType.php
@@ -87,7 +87,7 @@ class InstitutionFormType extends AbstractType
         $this->_add($builder, 'country', 'fancy_country', array('label' => 'Country', 'error_bubbling' => false));
         $this->_add($builder, 'city', 'city_list', array('label' => 'City' , 'error_bubbling' => false));
         $this->_add($builder, 'zipCode', 'text', array('label' => 'Zip / Postal Code'));
-        $this->_add($builder, 'state', 'text', array( 'error_bubbling' => false ,'label' => 'State / Province' ));
+        $this->_add($builder, 'state', 'state_list', array( 'error_bubbling' => false ,'label' => 'State / Province' ));
         $this->_add($builder, 'contactEmail', 'text', array('label' => 'Email Address ', 'required' => false));
         $this->_add($builder, 'address1', 'detailed_street_address', array('label' => 'Hospital Address'));
         $this->_add($builder, 'addressHint', 'text', array('label' => 'Helpful hint for getting there?', 'required' => false));
--- src/HealthCareAbroad/AdminBundle/Form/InstitutionMedicalCenterFormType.php
+++ src/HealthCareAbroad/AdminBundle/Form/InstitutionMedicalCenterFormType.php
@@ -84,7 +84,7 @@ class InstitutionMedicalCenterFormType extends AbstractType
         $this->_add($builder, 'name','text', array('label' => 'Name'));
         $this->_add($builder, 'description', 'textarea', array('label' => 'Short Description Of The Clinic', 'attr' => array('rows' => 4)));
         $this->_add($builder, 'businessHours', 'fancy_business_hours');
-        $this->_add($builder, 'state', 'text', array('label' => 'State or Province','disabled' => 'disabled', 'virtual' => true, 'attr' => array('value' => $this->institution->getState())));
+        $this->_add($builder, 'state', 'state_list', array('label' => 'State or Province','disabled' => 'disabled', 'virtual' => true));
         $this->_add($builder, 'contactEmail', 'text', array('label' => 'Email'));
         $this->_add($builder, 'contactDetails', 'collection', array('label' => 'Clinic Phone Number', 'type' => 'simple_contact_detail'));
         $this->_add($builder,'status', 'choice', array('label' => 'Status', 'choices' => $status));
