b73f0c14f8bd64b626190c082cbf6dcef4eac233
Allejo Chris Velarde <chris.velarde@chromedia.com>
2013-08-01 14:53:33 +0800

    Debugging code

--- web/app_dev.php
+++ web/app_dev.php
@@ -21,10 +21,35 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
-
+$mainStart = \microtime(true);
+$start = \microtime(true);
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
+$end = \microtime(true); 
+$initDiff = $end-$start;
+
+$start = \microtime(true);
 $response = $kernel->handle($request);
+$end = \microtime(true); 
+$responseDiff = $end-$start;  
+
+$start = \microtime(true);
+$startBeforeSend = \microtime(true);
+// echo "INIT: {$initDiff}s RESPONSE: {$responseDiff}s ";
+// exit;
 $response->send();
+
+$end = \microtime(true); 
+$responseSendDiff = $end-$start; 
+$total = $end-$mainStart;
+
 $kernel->terminate($request, $response);
+
+
+// if (defined('GLOBAL_WATA')){
+//     var_dump(GLOBAL_WATA);
+// }
+// echo "AA START TIME:{$startBeforeSend}s INIT: {$initDiff}s RESPONSE: {$responseDiff}s RESPONSE SEND: {$responseSendDiff}s TOTAL: {$total}s";
+// echo "<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>";
+// exit;
