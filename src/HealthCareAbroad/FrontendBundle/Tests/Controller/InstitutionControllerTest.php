53f19e593e6382d1011324d0585ff7c22bb7153f
Alnie Jacobe <alnie.jacobe@chromedia.com>
2013-08-05 14:22:36 +0800

    updated functional test for clinic and hospital profile page in frontend

--- src/HealthCareAbroad/FrontendBundle/Tests/Controller/InstitutionControllerTest.php
+++ src/HealthCareAbroad/FrontendBundle/Tests/Controller/InstitutionControllerTest.php
@@ -16,7 +16,7 @@ class InstitutionControllerTest extends FrontendBundleWebTestCase
         
         $this->assertGreaterThan(0,$crawler->filter('h3:contains("Hospital")')->count());
         $this->assertGreaterThan(0,$crawler->filter('img.featured-image')->count());
-        $this->assertGreaterThan(0,$crawler->filter('span.hospital-default-logo')->count());
+        $this->assertGreaterThan(0,$crawler->filter('img.hospital-logo')->count());
         
         //clinics section details
         $this->assertGreaterThan(0,$crawler->filter('h5:contains("Clinics")')->count());
@@ -31,7 +31,7 @@ class InstitutionControllerTest extends FrontendBundleWebTestCase
         $this->assertGreaterThan(0,$crawler->filter('h5:contains("Awards")')->count());
         $this->assertGreaterThan(0,$crawler->filter('li:contains("test")')->count());
         
-        //doctors sectiondetails
+        //doctors section details
         $this->assertGreaterThan(0,$crawler->filter('a:contains("DOCTORS")')->count());
         $this->assertGreaterThan(0,$crawler->filter('span.doctor-icon')->count());
         $this->assertGreaterThan(0,$crawler->filter('strong:contains("Dr. Pankaj  Chaturvedi")')->count());
@@ -42,7 +42,10 @@ class InstitutionControllerTest extends FrontendBundleWebTestCase
         $uri = "/hospital/test-single-hospital";
         $client = static::createClient();
         $crawler = $client->request('GET', $uri);
-        $this->assertGreaterThan(0,$crawler->filter('h3:contains("Hospital")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('h3:contains("Test Single Hospital")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('address:contains("Apollo Gleneagles Hospital, No. 58, Canal Circular Road 700054")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('html:contains("1 3323 203 040")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('span.clinic-default-logo')->count());
         
         
         //test invalid institutionSlug
