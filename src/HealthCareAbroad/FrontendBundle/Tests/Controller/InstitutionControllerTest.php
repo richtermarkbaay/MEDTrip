5a41571d00d06450a8c366c82150ae3b5bad8949
Alnie Jacobe <alnie.jacobe@chromedia.com>
2013-08-05 11:43:55 +0800

    updated functinal test for imc and institutionController

--- src/HealthCareAbroad/FrontendBundle/Tests/Controller/InstitutionControllerTest.php
+++ src/HealthCareAbroad/FrontendBundle/Tests/Controller/InstitutionControllerTest.php
@@ -13,14 +13,30 @@ class InstitutionControllerTest extends FrontendBundleWebTestCase
         $uri = "/hospital/ahalia-eye-hospital";
         $client = static::createClient();
         $crawler = $client->request('GET', $uri);
-        $this->assertGreaterThan(0,$crawler->filter('h3:contains("Hospital")')->count());
-        $this->assertGreaterThan(0,$crawler->filter('span:contains("Audiology Services")')->count());
-        $this->assertGreaterThan(0,$crawler->filter('h5:contains("Clinics")')->count());
-        $this->assertGreaterThan(0,$crawler->filter('h5:contains("Services")')->count());
-        $this->assertGreaterThan(0,$crawler->filter('h5:contains("Awards")')->count());
         
+        $this->assertGreaterThan(0,$crawler->filter('h3:contains("Hospital")')->count());
         $this->assertGreaterThan(0,$crawler->filter('img.featured-image')->count());
-//         $this->assertGreaterThan(0,$crawler->filter('span:contains("hospital-default-logo")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('span.hospital-default-logo')->count());
+        
+        //clinics section details
+        $this->assertGreaterThan(0,$crawler->filter('h5:contains("Clinics")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('span:contains("Audiology Services")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('span:contains("Pre-Admission Counselling and Evaluation (PACE) Clinic")')->count());
+        
+        //services section details
+        $this->assertGreaterThan(0,$crawler->filter('h5:contains("Services")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('li:contains("Text Message Reminders")')->count());
+        
+        //awards section details
+        $this->assertGreaterThan(0,$crawler->filter('h5:contains("Awards")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('li:contains("test")')->count());
+        
+        //doctors sectiondetails
+        $this->assertGreaterThan(0,$crawler->filter('a:contains("DOCTORS")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('span.doctor-icon')->count());
+        $this->assertGreaterThan(0,$crawler->filter('strong:contains("Dr. Pankaj  Chaturvedi")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('strong:contains("Dr. test  test")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('p:contains("Allergy and Immunology")')->count());
         
         
         $uri = "/hospital/test-single-hospital";
--- src/HealthCareAbroad/FrontendBundle/Tests/Controller/InstitutionMedicalCenterControllerTest.php
+++ src/HealthCareAbroad/FrontendBundle/Tests/Controller/InstitutionMedicalCenterControllerTest.php
@@ -13,10 +13,19 @@ class InstitutionMedicalCenterControllerTest extends FrontendBundleWebTestCase
         $uri = "/ahalia-eye-hospital/clinic/audiology-services";
         $client = static::createClient();
         $crawler = $client->request('GET', $uri);
+//         var_dump($crawler->filter('html'));exit;
         $this->assertGreaterThan(0,$crawler->filter('h2:contains("Ahalia Eye Hospital")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('h2:contains("Audiology Services")')->count());
         $this->assertGreaterThan(0,$crawler->filter('span.clinic-default-logo')->count());
         $this->assertGreaterThan(0,$crawler->filter('span.hospital-default-logo')->count());
-        $this->assertGreaterThan(0,$crawler->filter('h5.specialization')->count());
+        $this->assertEquals(0,$crawler->filter('img.featured-image')->count());
+        
+        $this->assertGreaterThan(0,$crawler->filter('h5:contains("Specializations")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('a:contains("Allergy and Immunology")')->count());
+        
+        $this->assertGreaterThan(0,$crawler->filter('h5:contains("Doctors")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('strong:contains("Dr. Pankaj  Chaturvedi")')->count());
+        $this->assertGreaterThan(0,$crawler->filter('p:contains("Allergy and Immunology")')->count());
         
         //test with invalid imc slug
         $uri = "/ahalia-eye-hospital/clinic/audiology-serviceses";
