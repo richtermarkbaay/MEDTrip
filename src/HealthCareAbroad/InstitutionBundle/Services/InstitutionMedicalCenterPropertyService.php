ef9f9ceabb4d874c2e51ee8afd857aa3d317ca54
Alnie Jacobe <alnie.jacobe@chromedia.com>
2013-08-20 14:04:02 +0800

    fixed bug on displaying globalAwards

--- src/HealthCareAbroad/InstitutionBundle/Services/InstitutionMedicalCenterPropertyService.php
+++ src/HealthCareAbroad/InstitutionBundle/Services/InstitutionMedicalCenterPropertyService.php
@@ -183,13 +183,13 @@ class InstitutionMedicalCenterPropertyService
             $returnVal = array();
             // get the property from the stored list
             foreach ($globalAwards as $_award) {
-//                 if (\array_key_exists($_award->getId(), $propertiesByValue) && \is_array($propertiesByValue[$_award->getId()])) {
-//                     foreach ($propertiesByValue[$_award->getId()] as $imp) {
-//                         // set the value object to GlobalAward
-//                         $imp->setValueObject($_award);
-//                         $returnVal[] = $imp;
-//                     }
-//                 }
+                if (\array_key_exists($_award->getId(), $propertiesByValue) && \is_array($propertiesByValue[$_award->getId()])) {
+                    foreach ($propertiesByValue[$_award->getId()] as $imp) {
+                        // set the value object to GlobalAward
+                        $imp->setValueObject($_award);
+                        $returnVal[] = $imp;
+                    }
+                }
             }
         }
 
