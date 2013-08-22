378e9a028771bc9ada9d13938d6a5e5661cd21c9
Harold Modesto <harold.modesto@chromedia.com>
2013-08-22 17:16:33 +0800

    fixed hca admin pagination links redirecting to dashboard

--- src/HealthCareAbroad/SearchBundle/Controller/AdminController.php
+++ src/HealthCareAbroad/SearchBundle/Controller/AdminController.php
@@ -19,77 +19,63 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
-	public function showWidgetAction($context)
-	{
-		if ('admin' == $context) {
-			$form = $this->createForm(new AdminDefaultSearchType());    		
-    		
-    		return $this->render('SearchBundle:Admin:searchWidget.html.twig', array('form' => $form->createView()));
-    	} else {
-    		throw new \Exception('Undefined context.');
-    	}
+    public function showWidgetAction($context)
+    {
+        if ('admin' == $context) {
+            $form = $this->createForm(new AdminDefaultSearchType());
+
+            return $this->render('SearchBundle:Admin:searchWidget.html.twig', array('form' => $form->createView()));
+        } else {
+            throw new \Exception('Undefined context.');
+        }
     }
-    
+
     /**
      * Main search function
      */
     public function initiateAction(Request $request)
     {
-        if (!$request->isMethod('POST')) {
-            return $this->redirect($this->generateUrl('admin_homepage'));
+        if ($request->isMethod('POST')) {
+            $searchCriteria = $request->get('adminDefaultSearch', array());
+        } else {
+            $searchCriteria['category'] = $request->get('category');
+            $searchCriteria['term'] = trim($request->get('term', ''));
         }
 
-		$searchCriteria = $request->get('adminDefaultSearch', array());
-		
-		if(!$searchCriteria){
-		    
-		    $searchCriteria['category'] = $request->get('category');
-		    $searchCriteria['term'] = \trim($request->get('term', ''));
-		}
-		
-		if($request->get('page')){
-		    $searchCriteria['page'] = $request->get('page');
-		}else{
-		    $searchCriteria['page'] = 1;
-		}
-		
-		$isDoctor = false;
-		$route = "";
-		switch ($searchCriteria['category']) {
-			case Constants::SEARCH_CATEGORY_INSTITUTION:
-				$varName = 'institutions';
-				break;
-			case Constants::SEARCH_CATEGORY_CENTER:
-				$varName = 'medicalCenters';
-				break;
+        $searchCriteria['page'] = $request->get('page', 1);
 
-			case Constants::SEARCH_CATEGORY_PROCEDURE_TYPE:
-				$varName = 'procedureTypes';
-				break;
-		
-			case Constants::SEARCH_CATEGORY_DOCTOR:
-				$varName = 'doctors';
-				$isDoctor = true;
-				break;
-				
-			case Constants::SEARCH_CATEGORY_SPECIALIZATION:
-			    $varName = 'specialization';
-			    break;
-			    
-			case Constants::SEARCH_CATEGORY_SUB_SPECIALIZATION:
-			    $varName = 'sub-specialization';
-				break;
-		}
-		$p = new SearchAdminPagerService();
-		$searchTerm = \trim($searchCriteria['term']);
-		$params = array(
-						"data" => $this->get('services.admin_search')->search($searchCriteria, $p),
-						"pager" => $p->getPager(),
-		                "isDoctor" => $isDoctor,
-		                "category" => $searchCriteria['category'],
-						"term" => $searchTerm
-		);
-		
-		return $this->render('SearchBundle:Admin:searchResult.html.twig',$params);
-	}
+        switch ($searchCriteria['category']) {
+            case Constants::SEARCH_CATEGORY_INSTITUTION:
+                $categoryName = 'Institutions';
+                break;
+            case Constants::SEARCH_CATEGORY_CENTER:
+                $categoryName = 'Medical Centers';
+                break;
+            case Constants::SEARCH_CATEGORY_PROCEDURE_TYPE:
+                $categoryName = 'Treatments';
+                break;
+            case Constants::SEARCH_CATEGORY_DOCTOR:
+                $categoryName = 'Doctors';
+                break;
+            case Constants::SEARCH_CATEGORY_SPECIALIZATION:
+                $categoryName = 'Specializations';
+                break;
+            case Constants::SEARCH_CATEGORY_SUB_SPECIALIZATION:
+                $categoryName = 'Sub-specializations';
+                break;
+            default:
+                $categoryName = '';
+        }
+
+        $p = new SearchAdminPagerService();
+
+        return $this->render('SearchBundle:Admin:searchResult.html.twig', array(
+            "data" => $this->get('services.admin_search')->search($searchCriteria, $p),
+            "pager" => $p->getPager(),
+            "isDoctor" => Constants::SEARCH_CATEGORY_DOCTOR == $searchCriteria['category'],
+            "category" => $searchCriteria['category'],
+            "categoryName" => $categoryName,
+            "term" => trim($searchCriteria['term'])
+        ));
+    }
 }
