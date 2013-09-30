2b8f1ba0df7587eea307d5c8f915692dc403e6ec
Chaztine Blance <chaztine.blance@chromedia.com>
2013-09-30 11:06:31 +0800

    WIP #646

--- src/HealthCareAbroad/TreatmentBundle/Services/TreatmentBundleService.php
+++ src/HealthCareAbroad/TreatmentBundle/Services/TreatmentBundleService.php
@@ -52,23 +52,23 @@ class TreatmentBundleService
      */
     public function getAllActiveSpecializations()
     {
+        
         if(static::$activeSpecializations) {
             return static::$activeSpecializations;
         }
-
+       
         // check in cache
         $key = 'TreatmentBundle:TreatmentBundleService:getAllActiveSpecializations';
         static::$activeSpecializations = $this->memcache->get($key);
 
         if (!static::$activeSpecializations) {
-            $result = $this->doctrine->getRepository('TreatmentBundle:Specialization')
+            static::$activeSpecializations = $this->doctrine->getRepository('TreatmentBundle:Specialization')
             ->getQueryBuilderForActiveSpecializations()
             ->getQuery()->getResult();
 
             // cache this result
             $this->memcache->set($key, static::$activeSpecializations);
         }
-
         return static::$activeSpecializations;
     }
 
