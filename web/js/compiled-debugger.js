8bf3a2aad1853847f1546fd68e69db7b1ddda7ff
Adelbert Silla <adelbert.silla@chromedia.com>
2013-10-18 10:29:24 +0800

    Updated compiled js.

--- web/js/compiled-debugger.js
+++ web/js/compiled-debugger.js
@@ -9502,10 +9502,11 @@ var HCA = {
 	{
 		elem.removeClass('error').find('ul').remove();
 	}
-	
-
 };
 
+/**
+ * @NOTE Do not Remove! This is being used in tinyMCE initialization at shared_parameters.yml
+ */
 function tinymceSetup(ed, e)
 {
 	// Tweak HTML5 Client Validation and apply it in tinymce editor.
@@ -9556,74 +9557,8 @@ function ucwords (str) {
 	});
 }
 
-
-HCA.autocomplete = {
-
-	init : function()
-	{
-		$('.autocomplete-medical-center, .autocomplete-procedure-type, .autocomplete-procedure').each(function(){
-			HCA.autocomplete.assignAutocomplete($(this));
-		});
-	},
-
-	split : function(val)
-	{
-		return val.split( /,\s*/ );
-	},
-
-	extractLast : function(term)
-	{
-		return HCA.autocomplete.split(term).pop();
-	},
-
-	assignAutocomplete : function(elem)
-	{
-		// don't navigate away from the field on tab when selecting an item
-		elem.bind( "keydown", function( event ) {
-			if ( event.keyCode === $.ui.keyCode.TAB &&
-					$( this ).data( "autocomplete" ).menu.active ) {
-				event.preventDefault();
-			}
-		})
-		.autocomplete({
-			source: function( request, response ) {
-				var params = {
-					section: elem.attr('class').split(' ').shift().replace('autocomplete-',''),
-					term: HCA.autocomplete.extractLast(elem.val())
-				}
-				$.getJSON(HCA.autocompleteSearchUrl, params, response);
-			},
-			search: function() {
-				// custom minLength
-				var term = HCA.autocomplete.extractLast( this.value );
-				if ( term.length < 2 ) {
-					return false;
-				}
-			},
-			focus: function() {
-				// prevent value inserted on focus
-				return false;
-			},
-			select: function( event, ui ) {
-				var terms = HCA.autocomplete.split( this.value );
-				// remove the current input
-				terms.pop();
-				// add the selected item
-				terms.push( ui.item.value );
-				// add placeholder to get the comma-and-space at the end
-				terms.push( "" );
-				this.value = terms.join( ", " );
-				return false;
-			}
-		});
-	}
-}
-
 $(function(){
 
-	// Initialize HCA autocomplete object.
-	HCA.autocomplete.init();
-	
 	// Confirmation Close button 
     $('#_close-confirmation-message').live('click', function(e) {
         $("#confirmation-message").fadeOut();
