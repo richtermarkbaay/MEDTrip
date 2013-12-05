58e709da196d02988c23f72e79705cb8811bd75c
Harold Modesto <harold.modesto@chromedia.com>
2013-12-05 11:56:04 +0800

    added expiration argument to set() of memcache service

--- src/HealthCareAbroad/MemcacheBundle/Services/MemcacheService.php
+++ src/HealthCareAbroad/MemcacheBundle/Services/MemcacheService.php
@@ -13,7 +13,7 @@ class MemcacheService
      * @var \Memcache
      */
     private $memcache;
-    
+
     public function __construct($servers=array())
     {
         if (\class_exists('Memcache')){
@@ -35,25 +35,25 @@ class MemcacheService
             $this->hasMemcache = false;
         }
     }
-    
+
     public function getMemcache()
     {
         return $this->memcache;
     }
-    
+
     public function getExtendedStats()
     {
         return $this->memcache->getExtendedStats();
     }
-    
+
     public function flush()
     {
         $this->memcache->flush();
     }
-    
+
     /**
      * Delete an item from the memcache server
-     * 
+     *
      * @param string $key
      * @return
      */
@@ -62,62 +62,62 @@ class MemcacheService
         if (!$this->hasMemcache) {
             return false;
         }
-        
+
         return $this->memcache->delete($key);
     }
-    
+
     /**
      * Increment the value of the given key
-     * 
+     *
      * @param string $key
      * @return boolean
      */
     public function increment($key)
     {
         if (!$this->hasMemcache) {
-            
+
             return false;
         }
-        
+
         $this->memcache->increment($key);
-        
+
         return true;
     }
-    
-    
+
+
     /**
      * Store data to memcached server. Return false if no Memcache is available.
-     * 
+     *
      * @param string $key
      * @param mixed $value
      * @return boolean|\HealthCareAbroad\MemcacheBundle\Services\MemcacheService
      */
-    public function set($key, $value)
+    public function set($key, $value, $expire = null)
     {
         if (!$this->hasMemcache) {
-            
+
             return false;
         }
-        
+
         // save the value to memcache
-        $this->memcache->set($key, $value);
-        
+        $this->memcache->set($key, $value, null, $expire);
+
         return true;
     }
-    
+
     /**
      * Get a stored value from memcached server
-     * 
+     *
      * @param string $key
      * @return Mixed
      */
     public function get($key)
     {
         if (!$this->hasMemcache) {
-            
+
             return false;
         }
-        
+
         return $this->memcache->get($key);
     }
 }
