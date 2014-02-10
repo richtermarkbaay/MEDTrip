d63a4fc4244543f186f212d231d3c5c1de3497fc
Allejo Chris Velarde <chris.velarde@chromedia.com>
2014-02-10 16:02:10 +0800

    WIP #675 - added button links

--- src/HealthCareAbroad/AdminBundle/Resources/public/js/treatment/view/treatment.view.js
+++ src/HealthCareAbroad/AdminBundle/Resources/public/js/treatment/view/treatment.view.js
@@ -4,16 +4,20 @@ var TreatmentView = Backbone.View.extend({
     className: '',
     
     render: function(){
+        var treatmentUrl = ApiUtility.getRootUrl()+'/admin/treatments/'+this.model.get('id')+'/edit'
         var nameCol = $('<td class="col-name"></td>').html(this.model.get('name'));
         
-        var subs = [];
+        var subSpecializationsCol = $('<td class="col-sub-specializations"></td>').append($('<ol></ol>'));
+        
         _.each(this.model.get('subSpecializations'), function(item, key, list){
-            subs.push(item.name);
+            var url = ApiUtility.getRootUrl()+'/admin/sub-specializations/'+item.id+'/edit';
+            subSpecializationsCol.find('ol').append($('<a href="'+url+'"></a>').html(item.name));
         });
-        var subSpecializationsCol = $('<td class="col-sub-specializations"></td>').html(subs.join(', '));
+        
         var descriptionCol = $('<td class="col-description"></td>').html(this.model.get('description'));
         var statusCol = $('<td class="col-status"></td>').html(this.model.getStatusLabel());
         var actionsCol = $('<td class="col-actions"></td>');
+        actionsCol.append($('<a href="'+treatmentUrl+'" class="btn"></a>').html('View'));
         
         this.$el
             .append(nameCol)
